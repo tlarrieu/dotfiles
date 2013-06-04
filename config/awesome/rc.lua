0ef6971d0b2246e05345477561ee323669f3d33c
smockey <thomas.larrieu@gmail.com>
2013-06-04 09:52:14 +0200

    fixing awesome's rc.lua. Gotta find a way to make my different environments collaborate (branch?)

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -11,7 +11,7 @@ require("naughty")
 
 -- This is used later as the default terminal and editor to run.
 -- terminal = "x-terminal-emulator"
-terminal = "terminal"
+terminal = "xfce4-terminal"
 editor = os.getenv("EDITOR") or "editor"
 editor_cmd = terminal .. " -e " .. editor
 -- Default modkey.
