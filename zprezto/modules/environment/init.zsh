86d1952b719020d45401ffa3a346c47bc6092e20
smockey <thomas.larrieu@gmail.com>
2013-06-13 15:40:47 +0200

    Changed highlighting color for grep

--- zprezto/modules/environment/init.zsh
+++ zprezto/modules/environment/init.zsh
@@ -38,7 +38,7 @@ unsetopt CHECK_JOBS       # Don't report on jobs when shell exit.
 #
 
 if zstyle -t ':prezto:environment:grep' color; then
-  export GREP_COLOR='37;42'
+  export GREP_COLOR='37;41'
   export GREP_OPTIONS='--color=auto -T -n'
 fi
 
