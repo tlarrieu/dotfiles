5d3d11ac24accb4372d17ce3028332fa967c93b6
smockey <thomas.larrieu@gmail.com>
2013-06-23 13:43:38 +0000

    fixing border size

--- config/awesome/themes/awesome-solarized/dark/theme.lua
+++ config/awesome/themes/awesome-solarized/dark/theme.lua
@@ -47,7 +47,7 @@ theme.bg_systray = theme.bg_normal
 -- }}}
 
 -- {{{ Borders
-theme.border_width  = "3"
+theme.border_width  = "2"
 theme.border_normal = theme.bg_normal
 theme.border_focus  = theme.colors.green
 theme.border_marked = theme.bg_urgent
