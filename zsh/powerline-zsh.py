c53f03cde9db1084ab13fafe850d55d3d606363a
Thomas Larrieu <thomas.larrieu@tiolive.com>
2013-06-30 20:40:20 +0000

    Tweaked prompt

--- zsh/powerline-zsh.py
+++ zsh/powerline-zsh.py
@@ -295,13 +295,11 @@ def add_root_indicator(powerline, error, running_jobs):
         bg = Color.CMD_FAILED_BG
 
     root_str = ''
-    if running_jobs:
-        root_str += ' ⚙ '
     user = subprocess.Popen(['whoami'], stdout=subprocess.PIPE).communicate()[0]
-    if user == "root":
-      root_str += ' #'
-    else:
-      root_str += ' $'
+    if user.rstrip() == "root":
+      root_str += ' ⚡'
+    if running_jobs:
+        root_str += ' ⚙'
 
     powerline.append(Segment(powerline, root_str, fg, bg))
 
