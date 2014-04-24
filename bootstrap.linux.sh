1136faf984b45ae0f193973e23dbbf9820f232c7
Thomas Larrieu <thomas.larrieu@gmail.com>
2014-04-25 01:14:35 +0200

    Cleaned up boostrap.linux.sh a little bit.

--- bootstrap.linux.sh
+++ bootstrap.linux.sh
@@ -55,16 +55,6 @@ fi
 [[ -d ~/.oh-my-fish ]] || curl -L https://github.com/bpinto/oh-my-fish/raw/master/tools/install.sh | sh
 safelink $BASEDIR/fish_theme/smockey $HOME/.oh-my-fish/themes/smockey
 
-# # .zprezto
-# safelink $BASEDIR/zprezto $HOME/.zprezto
-#
-# # zsh / zprezto dotfiles
-# for file in `ls $BASEDIR/zprezto/runcoms/z*`; do
-#   target=$BASEDIR/zprezto/runcoms/`basename $file`
-#   link=~/.`basename $file`
-#   safelink $target $link;
-# done
-
 # .moc
 safelink $BASEDIR/moc $HOME/.moc
 
