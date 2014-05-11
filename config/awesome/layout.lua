62c072d3c257d92b88a782e37fc117190d2ee23b
Thomas Larrieu <thomas.larrieu@gmail.com>
2014-05-11 04:49:57 +0200

    Restored date and window titles.

--- config/awesome/layout.lua
+++ config/awesome/layout.lua
@@ -98,14 +98,13 @@ for s = 1, screen.count() do
 
   -- Widgets that are aligned to the right
   local right_layout = wibox.layout.fixed.horizontal()
-  right_layout:add(powerline_widget)
-  --right_layout:add(mytextclock)
+  right_layout:add(mytextclock)
   right_layout:add(mylayoutbox[s])
 
   -- Now bring it all together (with the tasklist in the middle)
   local layout = wibox.layout.align.horizontal()
   layout:set_left(left_layout)
-  --layout:set_middle(mytasklist[s])
+  layout:set_middle(mytasklist[s])
   layout:set_right(right_layout)
 
   mywibox[s]:set_widget(layout)
