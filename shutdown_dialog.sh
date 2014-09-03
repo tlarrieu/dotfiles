43494e5118f3ac9de31ee51b8b236003fbfe394e
Thomas Larrieu <thomas.larrieu@gmail.com>
2014-09-03 20:03:19 +0200

    Better shutdown dialog.

--- shutdown_dialog.sh
+++ shutdown_dialog.sh
@@ -8,28 +8,16 @@ if [ -n "${ACTION}" ];then
     echo "awesome.quit()" | awesome-client
     ;;
   Shutdown)
-    zenity --question --text "Are you sure you want to halt?" && gksudo halt
-    ## or via ConsoleKit
-    # dbus-send --system --dest=org.freedesktop.ConsoleKit.Manager \
-    # /org/freedesktop/ConsoleKit/Manager org.freedesktop.ConsoleKit.Manager.Stop
+    zenity --question --text "Are you sure you want to halt?" && systemctl poweroff
     ;;
   Reboot)
-    zenity --question --text "Are you sure you want to reboot?" && gksudo reboot
-    ## Or via ConsoleKit
-    # dbus-send --system --dest=org.freedesktop.ConsoleKit.Manager \
-    # /org/freedesktop/ConsoleKit/Manager org.freedesktop.ConsoleKit.Manager.Restart
+    zenity --question --text "Are you sure you want to reboot?" && systemctl reboot
     ;;
   Suspend)
-    #gksudo pm-suspend
-    dbus-send --system --print-reply --dest=org.freedesktop.Hal \
-    /org/freedesktop/Hal/devices/computer \
-    org.freedesktop.Hal.Device.SystemPowerManagement.Suspend int32:0
-    # HAL is deprecated in newer systems in favor of UPower etc.
-    # dbus-send --system --dest=org.freedesktop.UPower /org/freedesktop/UPower org.freedesktop.UPower.Suspend
+    systemctl suspend
     ;;
   LockScreen)
     slock
-    # Or gnome-screensaver-command -l
     ;;
   esac
 fi
