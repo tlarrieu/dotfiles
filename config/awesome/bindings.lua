039a9e6025168573ffb6b7ba01d8ff1858864b78
Thomas Larrieu <thomas.larrieu@gmail.com>
2014-11-09 01:25:38 +0100

    Slight change to dmenu launching options

--- config/awesome/bindings.lua
+++ config/awesome/bindings.lua
@@ -118,7 +118,8 @@ globalkeys = awful.util.table.join(globalkeys,
     -- Solarized dark
     -- awful.util.spawn("dmenu_run -z -l 5 -i -p 'Run: ' -nb '#002b36' -nf '#839496' -sb '#073642' -sf '#859900' -fn '-*-terminus-medium-*-*-*-16-*-*-*-*-*-*-*'")
     -- Solarized light
-    awful.util.spawn("dmenu_run -z -l 5 -i -p 'Run: ' -nb '#fdf6e3' -nf '#657b83' -sb '#eee8d5' -sf '#859900' -fn '-*-terminus-medium-*-*-*-16-*-*-*-*-*-*-*'")
+    -- awful.util.spawn("dmenu_run -z -l 5 -i -p 'Run: ' -nb '#fdf6e3' -nf '#657b83' -sb '#eee8d5' -sf '#859900' -fn '-*-terminus-medium-*-*-*-16-*-*-*-*-*-*-*'")
+    awful.util.spawn("dmenu_run -z -l 5 -i -nb '#fdf6e3' -nf '#657b83' -sb '#eee8d5' -sf '#859900' -fn 'Inconsolata-12:normal'")
   end),
   -- Session control
   -- awful.key({ modkey            }, "q",           function () awful.util.spawn("xfce4-session-logout")       end),
