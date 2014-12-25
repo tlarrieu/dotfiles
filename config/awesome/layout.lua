7522132eb0df1f88b84e2ffbc45a9253becdb366
Thomas Larrieu <thomas.larrieu@gmail.com>
2014-12-25 22:58:37 +0100

    Code style

--- config/awesome/layout.lua
+++ config/awesome/layout.lua
@@ -1,7 +1,7 @@
 -- Themes define colours, icons, and wallpapers
 beautiful.init("/home/tlarrieu/.config/awesome/themes/awesome-solarized/light/theme.lua")
 
--- Wallpapers
+-- {{{ Wallpapers
 wallpapers = {
   "/home/tlarrieu/Pictures/Wallpapers/transistor/transistor-2.jpg",
   "/home/tlarrieu/Pictures/Wallpapers/transistor/transistor-3.jpg",
@@ -10,10 +10,10 @@ wallpapers = {
 for s = 1, screen.count() do
   gears.wallpaper.maximized(wallpapers[s], s, false)
 end
+-- }}}
 
--- Table of layouts to cover with awful.layout.inc, order matters.
-layouts =
-{
+-- {{{ Table of layouts to cover with awful.layout.inc, order matters.
+layouts = {
   awful.layout.suit.tile,
   awful.layout.suit.tile.left,
   awful.layout.suit.tile.bottom,
@@ -45,40 +45,51 @@ mysystray = wibox.widget.systray()
 mywibox = {}
 mypromptbox = {}
 mylayoutbox = {}
+
 mytaglist = {}
 mytaglist.buttons = awful.util.table.join(
-                      awful.button({        }, 1, awful.tag.viewonly),
-                      awful.button({ modkey }, 1, awful.client.movetotag),
-                      awful.button({        }, 3, awful.tag.viewtoggle),
-                      awful.button({ modkey }, 3, awful.client.toggletag),
-                      awful.button({        }, 4, awful.tag.viewnext),
-                      awful.button({        }, 5, awful.tag.viewprev)
-                    )
+  awful.button({        }, 1, awful.tag.viewonly),
+  awful.button({ modkey }, 1, awful.client.movetotag),
+  awful.button({        }, 3, awful.tag.viewtoggle),
+  awful.button({ modkey }, 3, awful.client.toggletag),
+  awful.button({        }, 4, awful.tag.viewnext),
+  awful.button({        }, 5, awful.tag.viewprev)
+)
+
 mytasklist = {}
 mytasklist.buttons = awful.util.table.join(
-                     awful.button({ }, 1, function (c)
-                                            if not c:isvisible() then
-                                              awful.tag.viewonly(c:tags()[1])
-                                            end
-                                            client.focus = c
-                                            c:raise()
-                                          end),
-                     awful.button({ }, 3, function ()
-                                            if instance then
-                                              instance:hide()
-                                              instance = nil
-                                            else
-                                              instance = awful.menu.clients({ width=250 })
-                                            end
-                                          end),
-                     awful.button({ }, 4, function ()
-                                            awful.client.focus.byidx(1)
-                                            if client.focus then client.focus:raise() end
-                                          end),
-                     awful.button({ }, 5, function ()
-                                            awful.client.focus.byidx(-1)
-                                            if client.focus then client.focus:raise() end
-                                          end))
+  awful.button({}, 1,
+    function (c)
+      if not c:isvisible() then
+        awful.tag.viewonly(c:tags()[1])
+      end
+      client.focus = c
+      c:raise()
+    end
+  ),
+  awful.button({}, 3,
+    function ()
+      if instance then
+        instance:hide()
+        instance = nil
+      else
+        instance = awful.menu.clients({ width=250 })
+      end
+    end
+  ),
+  awful.button({}, 4,
+    function ()
+      awful.client.focus.byidx(1)
+      if client.focus then client.focus:raise() end
+    end
+  ),
+  awful.button({}, 5,
+    function ()
+      awful.client.focus.byidx(-1)
+      if client.focus then client.focus:raise() end
+    end
+  )
+)
 
 for s = 1, screen.count() do
   -- Create a promptbox for each screen
@@ -86,11 +97,14 @@ for s = 1, screen.count() do
   -- Create an imagebox widget which will contains an icon indicating which layout we're using.
   -- We need one layoutbox per screen.
   mylayoutbox[s] = awful.widget.layoutbox(s)
-  mylayoutbox[s]:buttons(awful.util.table.join(
-                         awful.button({ }, 1, function () awful.layout.inc(layouts, 1) end),
-                         awful.button({ }, 3, function () awful.layout.inc(layouts, -1) end),
-                         awful.button({ }, 4, function () awful.layout.inc(layouts, 1) end),
-                         awful.button({ }, 5, function () awful.layout.inc(layouts, -1) end)))
+  mylayoutbox[s]:buttons(
+    awful.util.table.join(
+      awful.button({ }, 1, function () awful.layout.inc(layouts, 1) end),
+      awful.button({ }, 3, function () awful.layout.inc(layouts, -1) end),
+      awful.button({ }, 4, function () awful.layout.inc(layouts, 1) end),
+      awful.button({ }, 5, function () awful.layout.inc(layouts, -1) end)
+    )
+  )
   -- Create a taglist widget
   --mytaglist[s] = awful.widget.taglist(s, awful.widget.taglist.label.all, mytaglist.buttons)
   mytaglist[s] = awful.widget.taglist(s, awful.widget.taglist.filter.all, mytaglist.buttons)
