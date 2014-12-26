4b1842c76ee8547f19de1495695e38c112a8b877
Thomas Larrieu <thomas.larrieu@gmail.com>
2014-12-26 12:55:38 +0100

    Change wallpapers

--- config/awesome/layout.lua
+++ config/awesome/layout.lua
@@ -3,9 +3,9 @@ beautiful.init("/home/tlarrieu/.config/awesome/themes/awesome-solarized/light/th
 
 -- {{{ Wallpapers
 wallpapers = {
-  "/home/tlarrieu/Pictures/Wallpapers/transistor/transistor-2.jpg",
-  "/home/tlarrieu/Pictures/Wallpapers/transistor/transistor-3.jpg",
-  "/home/tlarrieu/Pictures/Wallpapers/hatsune_miku/Vocaloid-Anime-Hatsune-Miku-Append-tsunekun-664585.jpeg",
+  "/home/tlarrieu/Pictures/Wallpapers/desert.png",
+  "/home/tlarrieu/Pictures/Wallpapers/ahthebarensplit.jpg",
+  "/home/tlarrieu/Pictures/Wallpapers/hatsune_miku/wallpaper-1355730.jpg",
 }
 for s = 1, screen.count() do
   gears.wallpaper.maximized(wallpapers[s], s, false)
