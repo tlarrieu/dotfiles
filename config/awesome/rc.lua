8a41501d95eb5ab530b95fd128f9db1910a86f50
Thomas Larrieu <thomas.larrieu@gmail.com>
2015-03-31 00:07:37 +0200

    Reorganize rc.lua

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -18,10 +18,6 @@ terminal_exec = terminal .. " -e "
 editor = os.getenv("EDITOR") or "editor"
 editor_cmd = terminal_exec .. editor
 
--- Xcape
-awful.util.spawn_with_shell("killall xcape")
-awful.util.spawn_with_shell("xcape -e 'Control_L=Escape'")
-
 -- Default modkey.
 -- Usually, Mod4 is the key with a logo between Control and Alt.
 -- If you do not like this or do not have such a key,
@@ -30,6 +26,22 @@ awful.util.spawn_with_shell("xcape -e 'Control_L=Escape'")
 modkey = "Mod4"
 
 -- }}}
+
+-- {{{ Startup
+
+-- Xcape
+awful.util.spawn_with_shell("killall xcape")
+awful.util.spawn_with_shell("xcape -e 'Control_L=Escape'")
+
+-- Xflux
+awful.util.spawn_with_shell("killall xflux")
+awful.util.spawn_with_shell("xflux -l 48.8562213 -g 2.3486073") -- Paris coordinates
+
+-- ssh keychain
+awful.util.spawn_with_shell("keychain --eval --agents ssh -Q --quiet ~/.ssh/id_rsa")
+
+-- }}}
+
 
 require "layout"
 require "bindings"
