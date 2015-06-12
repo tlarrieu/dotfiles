121b641ac03a0f72ebbe11ad5fa6c0516ae9e25a
Thomas Larrieu <thomas.larrieu@gmail.com>
2015-06-13 01:15:38 +0200

    Kill processes through FZF

--- config/fish/functions/fish_user_key_bindings.fish
+++ config/fish/functions/fish_user_key_bindings.fish
@@ -1,1 +1,14 @@
 fzf_key_bindings
+
+function fkill
+  echo '' > /tmp/fzf.kill
+  ps ux | sed 1d | fzf -m | awk '{print $2}' > /tmp/fzf.kill
+
+  kill -9 (cat /tmp/fzf.kill) 2> /dev/null
+end
+
+bind \et 'fkill'
+
+if bind -M insert > /dev/null 2>&1
+  bind -M insert \et 'fkill'
+end
