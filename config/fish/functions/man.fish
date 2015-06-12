38f11e19aade4dea6aba13f6357ec78e02f01274
Thomas Larrieu <thomas.larrieu@gmail.com>
2015-06-13 01:15:15 +0200

    Remove useless escaping characters

--- config/fish/functions/man.fish
+++ config/fish/functions/man.fish
@@ -1,4 +1,4 @@
 function man
-  set PAGER /bin/sh\ -c\ \"unset\ PAGER\;col\ -b\ -x\ \|\ nvim\ -c\ \'set\ ft=man\ nomod\ nolist\'\ -\"
+  set PAGER /bin/sh -c "unset PAGER;col -b -x | nvim -c 'set ft=man nomod nolist' -"
   command man $argv | eval $PAGER
 end
