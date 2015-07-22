ea76b322d9c962c9192aca6bf257578e28a2314a
Thomas Larrieu <thomas.larrieu@gmail.com>
2015-07-23 00:13:55 +0200

    Add an option to enable cursor change upon entering insert mode in nvim

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -174,6 +174,8 @@ set PGHOST localhost
 set -g -x GEDITOR gvim
 set -g -x EDITOR nvim
 set -g -x TERM xterm-256color
+# Enable cursor change when neovim enters insert mode
+set -g -x NVIM_TUI_ENABLE_CURSOR_SHAPE 1
 
 # Gopath
 set -x GOPATH ~/go
