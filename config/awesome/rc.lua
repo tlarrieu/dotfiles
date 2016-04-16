f8223ffb325f7d49770a7b913d33c9b4df3021ee
Thomas Larrieu <thomas.larrieu@gmail.com>
2016-04-16 22:54:59 +0900

    Slight tweeks

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -29,14 +29,10 @@ require "rules"
 -- awful.util.spawn_with_shell("killall xcape; xcape -e 'Control_L=Escape'")
 
 -- Xflux
--- awful.util.spawn_with_shell("xflux -l 48.8562213 -g 2.3486073") -- Paris coordinates
-awful.util.spawn_with_shell("xflux -l 35.6732615 -g 139.5699578") -- Tokyo coordinates
+-- awful.util.spawn_with_shell("killall xflux; xflux -l 48.8562213 -g 2.3486073") -- Paris coordinates
+awful.util.spawn_with_shell("killall xflux; xflux -l 35.6732615 -g 139.5699578") -- Tokyo coordinates
 -- Configure keys
 awful.util.spawn_with_shell("setxkbmap -option ctrl:nocaps; xmodmap ~/.Xmodmap")
 
 -- Configure screen
 awful.util.spawn_with_shell("xrandr --output eDP1 --mode 1920x1080")
-
--- Touchpad
--- Activate touch to click
-awful.util.spawn_with_shell("xinput set-prop 'ETPS/2 Elantech Touchpad' 'libinput Tapping Enabled' 1")
