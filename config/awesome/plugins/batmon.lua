b843b5062420a5f1feaa6ce29cb27d87885ad503
Thomas Larrieu <thomas.larrieu@gmail.com>
2016-04-17 10:00:28 +0900

    Handle more cases + cleanup code

--- config/awesome/plugins/batmon.lua
+++ config/awesome/plugins/batmon.lua
@@ -5,10 +5,11 @@
 local naughty = require("naughty")
 local beautiful = require("beautiful")
 
-function readBatFile(adapter, ...)
+function readBattery(adapter, ...)
   local basepath = "/sys/class/power_supply/"..adapter.."/"
   for i, name in pairs({...}) do
-    file = io.open(basepath..name, "r")
+    local file = io.open(basepath..name, "r")
+
     if file then
       local str = file:read()
       file:close()
@@ -18,27 +19,19 @@ function readBatFile(adapter, ...)
 end
 
 function batteryInfo(adapter)
-  local battery_state = {
-      ["Full\n"]        = "↯",
-      ["Unknown\n"]     = "⌁",
-      ["Charged\n"]     = "↯",
-      ["Charging\n"]    = "+",
-      ["Discharging\n"] = "−"
-  }
+  local battery = "A/C"
+  local icon = ""
+  local percent = ""
+  local time = ""
 
-  local fh = io.open("/sys/class/power_supply/"..adapter.."/present", "r")
-  if fh == nil then
-    battery = "A/C"
-    icon = ""
-    percent = ""
-  else
-    local remaining = readBatFile(adapter, "charge_now", "energy_now")
-    local capacity = readBatFile(adapter, "charge_full", "energy_full")
-    local status = readBatFile(adapter, "status")
+  if io.open("/sys/class/power_supply/"..adapter.."/present", "r") ~= nil then
+    local remaining = readBattery(adapter, "charge_now", "energy_now")
+    local capacity = readBattery(adapter, "charge_full", "energy_full")
+    local status = readBattery(adapter, "status")
     battery = math.floor(remaining * 100 / capacity)
 
-    local current_now = readBatFile(adapter, "current_now", "power_now")
-    rate = tonumber(current_now)
+    local current_now = readBattery(adapter, "current_now", "power_now")
+    local rate = tonumber(current_now)
 
     if status:match("Charging") then
       icon = "↑"
@@ -50,20 +43,26 @@ function batteryInfo(adapter)
       timeleft = tonumber(remaining) / tonumber(rate)
 
       if tonumber(battery) < 15 then
+        local color = ""
+
         if tonumber(battery) <= 5 then
           color = "red"
         else
           color = "orange"
         end
+
         naughty.notify(
-          { title    = "Battery low"
-          , text     = "<br/><span color = '"..color.."'>"..battery..percent.." ".."left!</span>"
-          , timeout  = 5
+          { title = "Battery low"
+          , text = "<br/><span color = '"..color.."'>"..battery..percent.." ".."left!</span>"
+          , timeout = 5
           , position = "top_right"
-          , fg       = beautiful.fg_focus
-          , bg       = beautiful.bg_focus }
+          , fg = beautiful.fg_focus
+          , bg = beautiful.bg_focus }
         )
       end
+    elseif status:match("Full") then
+      icon = "↯"
+      percent = "%"
     else
       battery = "N/A"
       icon = "⌁"
@@ -73,11 +72,9 @@ function batteryInfo(adapter)
 
   if timeleft ~= nil then
     local hoursleft   = math.floor(timeleft)
-    local minutesleft = math.floor((timeleft - hoursleft) * 60 )
+    local minutesleft = math.floor((timeleft - hoursleft) * 60)
 
     time = string.format(" (%02d:%02d)", hoursleft, minutesleft)
-  else
-    time = ""
   end
 
   return battery..percent.." "..icon..time
