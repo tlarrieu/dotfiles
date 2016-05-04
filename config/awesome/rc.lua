9a07381e69384a7c2797065ed448803f1c91ba16
Thomas Larrieu <thomas.larrieu@gmail.com>
2016-05-05 00:03:12 +0200

    Tweak a few spawned commands

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -26,11 +26,11 @@ require "rules"
 --[[ Startup ]]-----------------------------------------------------------------
 
 -- Xcape
--- awful.util.spawn_with_shell("killall xcape; xcape -e 'Control_L=Escape'")
+awful.util.spawn_with_shell("killall xcape; xcape -e 'Control_L=Escape'")
 
 -- Xflux
--- awful.util.spawn_with_shell("killall xflux; xflux -l 48.8562213 -g 2.3486073") -- Paris coordinates
-awful.util.spawn_with_shell("killall xflux; xflux -l 35.6732615 -g 139.5699578") -- Tokyo coordinates
+awful.util.spawn_with_shell("killall xflux; xflux -l 48.8562213 -g 2.3486073") -- Paris coordinates
+-- awful.util.spawn_with_shell("killall xflux; xflux -l 35.6732615 -g 139.5699578") -- Tokyo coordinates
 -- Configure keys
 awful.util.spawn_with_shell("setxkbmap -option ctrl:nocaps; xmodmap ~/.Xmodmap")
 
