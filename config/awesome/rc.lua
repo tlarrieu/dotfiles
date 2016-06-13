ba4547b89c04de309049759f72095742a9d67300
Thomas Larrieu <thomas.larrieu@gmail.com>
2016-06-14 01:59:21 +0200

    Change xflux for redshift

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -25,9 +25,8 @@ require "rules"
 
 --[[ Startup ]]-----------------------------------------------------------------
 
--- Xflux
-awful.util.spawn_with_shell("killall xflux; xflux -l 48.8562213 -g 2.3486073") -- Paris coordinates
--- awful.util.spawn_with_shell("killall xflux; xflux -l 35.6732615 -g 139.5699578") -- Tokyo coordinates
+-- Redshift
+awful.util.spawn_with_shell("killall redshift; redshift")
 
 -- Configure keys
 awful.util.spawn_with_shell("killall xcape; xcape -e 'Control_L=Escape'")
