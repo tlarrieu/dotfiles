537b18aa41eef1108935dd6cd7a8854d50ec594b
Thomas Larrieu <thomas.larrieu@gmail.com>
2016-08-30 22:56:54 +0200

    Simplify rules
    - Remove outdated rules (for software we do not use anymore)
    - Merge many rules into a single one with "rule_any"

--- config/awesome/rules.lua
+++ config/awesome/rules.lua
@@ -1,5 +1,4 @@
 awful.rules.rules = {
-  -- All clients will match this rule.
   {
     rule = {},
     properties = {
@@ -7,60 +6,35 @@ awful.rules.rules = {
       border_color = beautiful.border_normal,
       focus = true,
       keys = clientkeys,
-      buttons = clientbuttons,
-      opacity = 0.9
+      buttons = clientbuttons
     }
+  }, -- Default rules for all applications
+
+  {
+    rule = { class = "Plugin-container" },
+    properties = {
+      floating = true,
+      ontop = true,
+      border = 0
+    }
+  }, -- Flash player
+
+  {
+    rule_any = {
+      class = {
+        "Gpick",
+        "Gvba",
+        "MPlayer",
+        "Vlc",
+        "mednafen",
+        "mpv",
+      }
+    },
+    properties = { floating = true, ontop = true }
   },
 
-  {rule = {class = "Firefox"}, properties = {opacity = 1.0}},
-  {rule = {class = "Opera"}, properties = {opacity = 1.0}},
-  {rule = {class = "luakit"}, properties = {opacity = 1.0}},
-  {rule = {class = "Plugin-container"}, properties = {
-    opacity = 1.0,
-    floating = true,
-    ontop = true,
-    border = 0
-  }}, -- Flash player
-
-  {rule = {class = "Evince"}, properties = {opacity = 1.0}},
-  {rule = {class = "Zathura"}, properties = {opacity = 1.0}},
-
-  {rule = {class = "Gimp-2.8"}, properties = {opacity = 1.0}},
-  {rule = {class = "Inkscape"}, properties = {opacity = 1.0}},
-  {rule = {class = "feh"}, properties = {opacity = 1.0}},
-  {rule = {class = "Gnumeric"}, properties = {opacity = 1.0}},
-
-  {rule = {class = "Klavaro"}, properties = {opacity = 1.0}},
-
-  {rule = {class = "Wine"}, properties = {opacity = 1.0}},
-
-  {rule = {class = "Eclipse"}, properties = {opacity = 1.0}},
-
-  {rule = {class = "MPlayer"}, properties = {
-    opacity = 1.0, floating = true, ontop = true
-  }},
-  {rule = {class = "mpv"}, properties = {
-    opacity = 1.0, floating = true, ontop = true
-  }},
-  {rule = {class = "Vlc"}, properties = {
-    opacity = 1.0, floating = true, ontop = true
-  }},
-
-  {rule = {class = "mednafen"}, properties = {
-    opacity = 1.0, floating = true, ontop = true
-  }},
-  {rule = {class = "Phoenix"}, properties = {
-    opacity = 1.0, floating = true, ontop = true, fullscreen = true
-  }}, -- bsnes
-  {rule = {class = "Gvba"}, properties = {opacity = 1.0, floating = true}},
-  {rule = {class = "Minecraft"}, properties = {
-    opacity = 1.0, floating = true
-  }},
-
-  {rule = {class = "Conky"}, properties = {floating = true}},
-  {rule = {class = "Kupfer.py"}, properties = {border_width = 0}},
-
-  {rule = {class = "Gpick"}, properties = {
-    floating = true, opacity = 1.0, ontop = true
-  }}
+  {
+    rule = { class = "Minecraft" },
+    properties = { floating = true },
+  },
 }
