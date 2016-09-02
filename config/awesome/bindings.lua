89825b0ab3fe388bebbaaa6db2abce8a49978317
Thomas Larrieu <thomas.larrieu@gmail.com>
2016-09-02 19:24:05 +0200

    Change Spotify binding slightly

--- config/awesome/bindings.lua
+++ config/awesome/bindings.lua
@@ -229,7 +229,7 @@ keyboard = awful.util.table.join(keyboard,
 
   mtspawn("u", "ranger"),
   mtspawn("m", "mocp"),
-  spawn({modkey, "Shift"}, "m", "spotify"),
+  spawn({modkey, "Control"}, "m", "spotify"),
 
   mspawn("a", "pavucontrol"),
   mspawn("g", "thunar"),
