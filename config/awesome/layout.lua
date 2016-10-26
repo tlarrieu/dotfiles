fc1ae1597de5c55b3dd1746a0741c37080b43c95
Thomas Larrieu <thomas.larrieu@gmail.com>
2016-10-26 22:27:54 +0200

    Tweak overall layout

--- config/awesome/layout.lua
+++ config/awesome/layout.lua
@@ -3,62 +3,73 @@ local gears = require("gears")
 local beautiful = require("beautiful")
 local lain = require("lain")
 
--- [[ Widgets ]] ---------------------------------------------------------------
-
 local layoutbox = awful.widget.layoutbox(s)
 layoutbox:buttons(layoutbuttons)
 
-local clockwidget = awful.widget.textclock()
+local clockwidget = awful.widget.textclock(
+  lain.util.markup(beautiful.colors.base1, " %Y.%m.%d %H:%M")
+)
 
 local battextwidget = wibox.widget.textbox()
-
-baticon = wibox.widget.imagebox(beautiful.bat)
-batbar = awful.widget.progressbar()
+battextwidget:set_font("Inconsolata-g For Powerline 10")
+local baticon = wibox.widget.imagebox(beautiful.bat)
+local batbar = awful.widget.progressbar()
 batbar:set_width(45)
-batbar:set_ticks(true)
+batbar:set_ticks(false)
 batbar:set_ticks_size(5)
 batbar:set_background_color(beautiful.bg_normal)
-batmargin = wibox.layout.margin(batbar, 2, 7)
-batmargin:set_top(6)
-batmargin:set_bottom(6)
-
-batupd = lain.widgets.bat({
-  battery = "BAT1",
-  timeout = 15,
-  settings = function()
-    if bat_now.status == "N/A" then return end
-
-    battextwidget:set_text(bat_now.time .. " ")
-
-    if bat_now.status == "Charging" then
-      baticon:set_image(beautiful.ac)
-      if bat_now.perc >= 98 then
-        batbar:set_color(beautiful.colors.green)
-      elseif bat_now.perc > 50 then
-        batbar:set_color(beautiful.colors.base03)
-      elseif bat_now.perc > 15 then
-        batbar:set_color(beautiful.colors.yellow)
-      else
-        batbar:set_color(beautiful.colors.red)
-      end
-    else
-      if bat_now.perc > 50 then
-        batbar:set_color(beautiful.colors.green)
-        baticon:set_image(beautiful.bat)
-      elseif bat_now.perc > 15 then
-        batbar:set_color(beautiful.colors.yellow)
-        baticon:set_image(beautiful.bat_low)
-      else
-        batbar:set_color(beautiful.colors.red)
-        baticon:set_image(beautiful.bat_no)
-      end
-    end
-
-    batbar:set_value(bat_now.perc / 100)
-  end
-})
-batwidget = wibox.widget.background(batmargin)
+local batmargin = wibox.layout.margin(batbar, 2, 7)
+batmargin:set_top(8)
+batmargin:set_left(7)
+batmargin:set_bottom(8)
+local batwidget = wibox.widget.background(batmargin)
 batwidget:set_bgimage(beautiful.widget_bg)
+
+local batcallback = function()
+  if bat_now.status == "N/A" then return end
+
+  local color = nil
+  local legend = nil
+  local icon = nil
+
+  if bat_now.time == "00:00" then
+    legend = "100%"
+  else
+    legend = bat_now.time
+  end
+
+  if bat_now.status == "Charging" then
+    icon = beautiful.ac
+
+    if bat_now.perc >= 98 then
+      color = beautiful.colors.green
+    elseif bat_now.perc > 50 then
+      color = beautiful.fg_normal
+    elseif bat_now.perc > 15 then
+      color = beautiful.colors.yellow
+    else
+      color = beautiful.colors.red
+    end
+  else
+    if bat_now.perc > 50 then
+      color = beautiful.colors.green
+      icon = beautiful.bat
+    elseif bat_now.perc > 15 then
+      color = beautiful.colors.yellow
+      icon = beautiful.bat_low
+    else
+      color = beautiful.colors.red
+      icon = beautiful.bat_no
+    end
+  end
+
+  baticon:set_image(icon)
+  battextwidget:set_markup(lain.util.markup(color, legend))
+  batbar:set_color(color)
+  batbar:set_value(bat_now.perc / 100)
+end
+
+lain.widgets.bat({ battery = "BAT1", timeout = 15, settings = batcallback })
 
 for s = 1, screen.count() do
   gears.wallpaper.maximized(wallpaper, s, false)
--- config/awesome/themes/awesome-solarized/light/theme.lua
+++ config/awesome/themes/awesome-solarized/light/theme.lua
@@ -1,10 +1,9 @@
--- {{{ Main
+-- [[ main ]] ------------------------------------------------------------------
 theme = {}
 theme.path = os.getenv("HOME") .. "/.config/awesome/themes/awesome-solarized/light/"
 theme.default_themes_path = "/usr/share/awesome/themes"
--- }}}
 
--- {{{ Colors
+-- [[ palette ]] ---------------------------------------------------------------
 theme.colors = {}
 theme.colors.base03  = "#002b36"
 theme.colors.base02  = "#073642"
@@ -22,62 +21,54 @@ theme.colors.violet  = "#6c71c4"
 theme.colors.blue    = "#268bd2"
 theme.colors.cyan    = "#2aa198"
 theme.colors.green   = "#859900"
--- }}}
 
--- {{{ Styles
+-- [[ styles ]] ----------------------------------------------------------------
 theme.font      = "Inconsolata-g For Powerline 11"
 
--- {{{ Colors
-theme.fg_normal  = theme.colors.base02
-theme.fg_focus   = theme.colors.green
-theme.fg_urgent  = theme.colors.base3
+-- [[ colors ]] ----------------------------------------------------------------
+theme.fg_normal         = theme.colors.base1
+theme.fg_focus          = theme.colors.green
+theme.fg_urgent         = theme.colors.base3
 
-theme.tasklist_fg_focus = theme.colors.green
+theme.tasklist_fg_focus = theme.colors.base03
 
-theme.bg_normal  = theme.colors.base3
-theme.bg_focus   = theme.colors.base3
-theme.bg_urgent  = theme.colors.red
-theme.bg_systray = theme.bg_normal
--- }}}
+theme.bg_normal         = theme.colors.base3
+theme.bg_focus          = theme.colors.base3
+theme.bg_urgent         = theme.colors.red
+theme.bg_systray        = theme.bg_normal
 
--- {{{ Borders
+-- [[ borders ]] ---------------------------------------------------------------
 theme.border_width  = "2"
 theme.border_normal = theme.bg_normal
 theme.border_focus  = theme.colors.green
 theme.border_marked = theme.bg_urgent
--- }}}
 
--- {{{ Titlebars
+-- [[ titlebars ]] -------------------------------------------------------------
 theme.titlebar_bg_focus  = theme.bg_focus
 theme.titlebar_bg_normal = theme.bg_normal
--- }}}
 
--- {{{ Mouse finder
+-- [[ mouse finder ]] ----------------------------------------------------------
 theme.mouse_finder_color = theme.colors.cyan
 -- mouse_finder_[timeout|animate_timeout|radius|factor]
--- }}}
 
--- {{{ Menu
+-- [[ menu ]] ------------------------------------------------------------------
 -- Variables set for theming the menu:
 -- menu_[bg|fg]_[normal|focus]
 -- menu_[border_color|border_width]
 theme.menu_height = "18"
 theme.menu_width  = "200"
--- }}}
 
--- {{{ Icons
--- {{{ Taglist
+-- [[ icons ]] ----------------------------------------------------------------------
+-- taglist
 theme.taglist_squares_sel   = theme.path.."taglist/square_sel.png"
 theme.taglist_squares_unsel = theme.path.."taglist/square_unsel.png"
 theme.taglist_bg_focus      = "png:" .. theme.path .. "taglist/bg_focus.png"
--- }}}
 
--- {{{ Misc
-theme.awesome_icon           = theme.path.."/layouts/awesome-icon.png"
-theme.menu_submenu_icon      = theme.default_themes_path.."/default/submenu.png"
--- }}}
+-- misc
+theme.awesome_icon      = theme.path.."/layouts/awesome-icon.png"
+theme.menu_submenu_icon = theme.default_themes_path.."/default/submenu.png"
 
--- {{{ Layout
+-- layout
 theme.layout_tile       = theme.path.."layouts/tile.png"
 theme.layout_tileleft   = theme.path.."layouts/tileleft.png"
 theme.layout_tilebottom = theme.path.."layouts/tilebottom.png"
@@ -90,9 +81,8 @@ theme.layout_max        = theme.path.."layouts/max.png"
 theme.layout_fullscreen = theme.path.."layouts/fullscreen.png"
 theme.layout_magnifier  = theme.path.."layouts/magnifier.png"
 theme.layout_floating   = theme.path.."layouts/floating.png"
--- }}}
 
--- {{{ Titlebar
+-- titlebar
 theme.titlebar_close_button_focus               = theme.path.."titlebar/close_focus.png"
 theme.titlebar_close_button_normal              = theme.path.."titlebar/close_normal.png"
 
@@ -115,12 +105,11 @@ theme.titlebar_maximized_button_focus_active    = theme.path.."titlebar/maximize
 theme.titlebar_maximized_button_normal_active   = theme.path.."titlebar/maximized_normal_active.png"
 theme.titlebar_maximized_button_focus_inactive  = theme.path.."titlebar/maximized_focus_inactive.png"
 theme.titlebar_maximized_button_normal_inactive = theme.path.."titlebar/maximized_normal_inactive.png"
--- }}}
--- }}}
 
-theme.ac                                        = theme.path .. "/icons/ac.png"
-theme.bat                                       = theme.path .. "/icons/bat.png"
-theme.bat_low                                   = theme.path .. "/icons/bat_low.png"
-theme.bat_no                                    = theme.path .. "/icons/bat_no.png"
+-- [[ battery widget ]] --------------------------------------------------------
+theme.ac      = theme.path .. "/icons/ac.png"
+theme.bat     = theme.path .. "/icons/bat.png"
+theme.bat_low = theme.path .. "/icons/bat_low.png"
+theme.bat_no  = theme.path .. "/icons/bat_no.png"
 
 return theme
