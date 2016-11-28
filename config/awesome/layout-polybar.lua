8ed1344db49695affb06e5ea2097b6f532b2350e
Thomas Larrieu <thomas.larrieu@gmail.com>
2016-11-28 22:05:13 +0100

    Rename polybar specific layout

--- config/awesome/layout-polybar.lua
+++ config/awesome/layout-polybar.lua
@@ -4,7 +4,7 @@ local gears = require("gears")
 for s = 1, screen.count() do
   gears.wallpaper.maximized(wallpaper, s, false)
 
-  -- HACK to make room for lemonbuddy (transparent wibox that takes the
-  -- necessary screen estate)
+  -- HACK to make room for polybar (transparent wibox that takes the necessary
+  -- screen estate)
   local panel = awful.wibox({ position = "top", screen = s, bg = "#FFFFFF00" })
 end
