98e6aad6feebe90284a9f11318f2a5026bc66c1a
Thomas Larrieu <thomas.larrieu@gmail.com>
2017-08-21 13:37:16 +0200

    Restore rbenv initialization
    The fish plugin does not seem to set the path...

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -4,6 +4,11 @@ stty -ixon
 set -x FZF_DEFAULT_COMMAND "ag -g '' --hidden --ignore .git"
 set -x FZF_DEFAULT_OPTS '--reverse'
 
+# rbenv
+if type rbenv > /dev/null
+  source (rbenv init - | psub)
+end
+
 # Postgres
 set PGHOST localhost
 
@@ -16,5 +21,4 @@ set -x PATH $PATH $HOME/scripts
 set -x PATH $PATH $HOME/bin
 
 fundle plugin 'tuvistavie/fish-ssh-agent'
-fundle plugin 'fisherman/rbenv'
 fundle init
