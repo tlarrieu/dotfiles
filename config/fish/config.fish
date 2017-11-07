89109b10fddd3d14911d30b426fbb821d1c93f35
Thomas Larrieu <thomas.larrieu@gmail.com>
2017-11-07 17:39:53 +0100

    Add some configuration for FZF

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -4,6 +4,9 @@ stty -ixon
 set -x FZF_DEFAULT_COMMAND "ag -g '' --hidden --ignore .git"
 set -x FZF_DEFAULT_OPTS '--reverse'
 
+set -x FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
+set -x FZF_CTRL_T_OPTS $FZF_DEFAULT_OPTS
+
 # rbenv
 if type rbenv > /dev/null
   source (rbenv init - | psub)
