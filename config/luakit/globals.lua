662fec188e2e1dfd41dc13dcf95218fbbaf3e8cf
Thomas Larrieu <thomas.larrieu@gmail.com>
2019-01-20 17:14:37 +0100

    Trim down configuration
    We are preparing the “new” of configuring luakit

--- config/luakit/globals.lua
+++ config/luakit/globals.lua
@@ -1,6 +1,5 @@
 package.path = package.path .. ';/home/tlarrieu/scripts/?.lua'
 
--- Global variables for luakit
 local xrdb = require('xrdb')
 
 local globals = {
@@ -12,20 +11,9 @@ local globals = {
   default_window_size = "800x600",
   vertical_tab_width  = 200,
   default_zoom_level  = xrdb.apply_dpi(120, xrdb.load().font.dpi),
-
-  -- Disables loading of hostnames from /etc/hosts (for large host files)
-  -- load_etc_hosts      = false,
-  -- Disables checking if a filepath exists in search_open function
-  -- check_filepath      = false,
-  -- Specify your preferred terminal emulator
-  term                = "urxvt",
+  term                = "kitty",
 }
 
--- List of search engines. Each item must contain a single %s which is
--- replaced by URI encoded search terms. All other occurances of the percent
--- character (%) may need to be escaped by placing another % before or after
--- it to avoid collisions with lua's string.format characters.
--- See: http://www.lua.org/manual/5.1/manual.html#pdf-string.format
 globals.search_engines = {
   duckduckgo = "https://duckduckgo.com/?q=%s",
   g          = "https://github.com/%s",
@@ -39,33 +27,13 @@ globals.search_engines = {
   yt         = "https://www.youtube.com/results?search_query=%s",
 }
 
--- Set google as fallback search engine
 globals.search_engines.default = globals.search_engines.duckduckgo
--- Use this instead to disable auto-searching
---search_engines.default = "%s"
 
--- Per-domain webview properties
--- https://webkitgtk.org/reference/webkit2gtk/stable/WebKitWebView.html
--- https://webkitgtk.org/reference/webkit2gtk/stable/WebKitSettings.html
 globals.domain_props = {
-  ["all"] = {
-    enable_webgl               = true,
-    -- enable_scripts          = false,
-    -- enable_plugins          = false,
-    -- zoom_text_only          = true,
-  }, --[[
-  ["youtube.com"] = {
-    enable_scripts = true,
-    enable_plugins = true,
-  }, ]]
+  ["all"] = { enable_webgl = true, },
 }
 
--- Cookie acceptance policy
--- Acceptable values: "always", "never", "no_third_party"
 soup.accept_policy = "no_third_party"
-
--- Cookie storage location
--- Remove this line or set to nil to disable cookie persistence
 soup.cookies_storage = luakit.data_dir .. "/cookies.db"
 
 return globals
