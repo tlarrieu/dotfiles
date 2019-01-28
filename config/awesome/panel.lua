33db0de989ae0c4800cb5260e4f6d522bf6b2e8d
Thomas Larrieu <thomas.larrieu@gmail.com>
2019-01-28 21:55:10 +0100

    Remove unused variable

--- config/awesome/panel.lua
+++ config/awesome/panel.lua
@@ -1,6 +1,5 @@
 local awful = require("awful")
 local beautiful = require("beautiful")
-local gears = require("gears")
 local wibox = require("wibox")
 local dpi = require('beautiful.xresources').apply_dpi
 
