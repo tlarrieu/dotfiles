9c9a388f06cd99665847529cb9b3dac23a9141a9
Thomas Larrieu <thomas.larrieu@gmail.com>
2019-05-16 13:31:49 +0200

    Add seconds to the timestamp to allow making multiple screenshots the same minute

--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-filename="$HOME/Pictures/screenshots/$(date +'%Y%m%d%H%M').png"
+filename="$HOME/Pictures/screenshots/$(date +'%Y%m%d%H%M%S').png"
 maim -s | feh - --fullscreen --action "mv %F $filename"
