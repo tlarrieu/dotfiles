69a84f343b28697422edbdb399cab4a6436baba5
Thomas Larrieu <thomas.larrieu@gmail.com>
2020-06-04 09:40:59 +0200

    Add an appsignal app

--- config/awesome/signals.lua
+++ config/awesome/signals.lua
@@ -71,6 +71,7 @@ local rules = {
   { class = "Chromium", instance = "pinterest.com", icon = "" },
   { class = "Chromium", instance = "citymapper.com", icon = "" },
   { class = "Chromium", instance = "hub.docker.com", icon = "" },
+  { class = "Chromium", instance = "appsignal.com", icon = "勞" },
 
   { class = "Chromium", instance = "roll20.net", icon = "﫩" },
   { class = "Chromium", instance = "1BLpUgFZ7HXxhe9sbIL9KdwTvQkLwSPTjyQwzwY6f", icon = "" },
