ecaa670ab9d87f7a180363ef397210185fafa3a4
Thomas Larrieu <thomas.larrieu@gmail.com>
2020-06-22 16:23:36 +0200

    Update aria2c target directory to current
    We let it still explicit to simplify on the fly changes

--- config/fish/conf.d/aliases.fish
+++ config/fish/conf.d/aliases.fish
@@ -46,7 +46,7 @@ abbr xo "xsel --clipboard -o"
 abbr xi "xsel --clipboard -i"
 
 # aria2c
-abbr dl "aria2c (xsel --clipboard -o) --dir=$HOME/Downloads"
+abbr dl "aria2c (xsel --clipboard -o) --dir=."
 
 # curl
 abbr c "curl"
