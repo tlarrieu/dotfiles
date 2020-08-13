44675a40d9a6c20050e40664d793fcd92774bc03
Thomas Larrieu <thomas.larrieu@gmail.com>
2020-08-13 16:35:46 +0200

    Bind ctrl-k by default in fzf (and give it readline behavior)

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -24,6 +24,7 @@ set -x FZF_DEFAULT_OPTS "
   --color=bg+:$color01,bg:$color00,spinner:$color0C,hl:$color0D
   --color=fg:$color04,header:$color0D,info:$color0A,pointer:$color0C
   --color=marker:$color0C,fg+:$color06,prompt:$color0A,hl+:$color0D
+  --bind=ctrl-k:kill-line
 "
 
 set -x FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
