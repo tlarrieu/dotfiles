d06afb9c402733789c3e33425bf3099943fe2b03
Thomas Larrieu <thomas.larrieu@gmail.com>
2020-08-13 16:02:47 +0200

    Handle all sort of actions in the rofi-pass script

--- config/awesome/bindings.lua
+++ config/awesome/bindings.lua
@@ -153,9 +153,7 @@ _M.keyboard = {
     mspawn("k",                    script("rofi-emojis")),
     mspawn("f",                    script("rofi-nerdfont")),
     mspawn("à",                    script("rofi-bluetooth")),
-
-    mspawn("Escape",                script("rofi-pass password")),
-    spawn({mod, "Shift"}, "Escape", script("rofi-pass login")),
+    mspawn("Escape",               script("rofi-pass")),
 
     mspawn("q", script("rofi-power")),
     mspawn("a", termstart("pulsemixer", { class = "mixer" })),
