96e20b1e96d8c5f93491b4f78d4c0174149fdcd6
Thomas Larrieu <thomas.larrieu@gmail.com>
2020-09-14 19:30:07 +0200

    Refine “myip” command

--- config/fish/functions/myip.fish
+++ config/fish/functions/myip.fish
@@ -3,10 +3,15 @@ function tab
 end
 
 function myip
-  echo "Local IP:"
-  /sbin/ifconfig | awk '/inet/ { print $2 } ' | sed -e s/adr:// | sed -e s/inet6:// | tab
+  set -l ip /usr/bin/ip
+  set -l curl /usr/bin/curl
+
+  set -l device ($ip route | head -n1 | awk '{ print $5 }')
+
+  echo "Local IP (guessing $device as the default device):"
+  $ip -4 a s $device | awk '/inet/ { print $2 } ' | tab
 
   echo
   echo "Public IP:"
-  curl https://api.ipify.org 2> /dev/null | tab
+  $curl https://api.ipify.org 2> /dev/null | tab
 end
