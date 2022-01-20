f135b594db0b5c8c321dd34c9a630d11eb7ce20e
Thomas Larrieu <thomas.larrieu@gmail.com>
2022-01-20 10:11:35 +0100

    Add an icon for amplitude

--- config/awesome/signals.lua
+++ config/awesome/signals.lua
@@ -96,6 +96,7 @@ local rules = {
   { class = "Chromium", instance = "hoogle", icon = "" },
   { class = "Chromium", instance = "docusign", icon = "" },
   { class = "Chromium", instance = "notion.so", icon = "" },
+  { class = "Chromium", instance = "amplitude.com", icon = "" },
 
   { class = "Chromium", instance = "roll20.net", icon = "﫩" },
 
