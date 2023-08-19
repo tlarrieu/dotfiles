7dc78d9636da0b6e13f4a106369d8bd4a004ec34
Thomas Larrieu <thomas.larrieu@gmail.com>
2023-08-19 22:01:52 +0200

    Automatically compile packer upon editing setup.lua

--- nvim/lua/plugins/setup.lua
+++ nvim/lua/plugins/setup.lua
@@ -1,4 +1,11 @@
-vim.cmd [[packadd packer.nvim]]
+vim.cmd([[
+  packadd packer.nvim
+
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+  augroup end
+]])
 
 return require('packer').startup(function(use)
   -- {{{ --| File Manipulation |--------------
