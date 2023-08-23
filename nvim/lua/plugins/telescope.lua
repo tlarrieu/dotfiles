95a6bc79f9590ec5de2a4a5c72c9898618bc7aad
Thomas Larrieu <thomas.larrieu@gmail.com>
2023-08-23 11:44:48 +0200

    Tweak telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -8,30 +8,34 @@ require('telescope').setup{
     selection_caret = ' ',
     multi_icon = ' ',
 
-    sorting_strategy = "ascending",
+    path_display = { 'smart', 'shorten' },
+
+    sorting_strategy = 'ascending',
     layout_config = {
-      prompt_position="top",
+      prompt_position = 'top',
+      preview_width = 0.55,
     },
 
     mappings = {
       i = {
-        ["<esc>"] = actions.close,
-        ["<c-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
+        ['<esc>'] = actions.close,
+        ['<c-q>'] = actions.smart_send_to_qflist + actions.open_qflist,
 
-        ["<c-a>"] = { "<home>", type = "command" },
-        ["<c-e>"] = { "<end>", type = "command" },
+        ['<c-a>'] = { '<home>', type = 'command' },
+        ['<c-e>'] = { '<end>', type = 'command' },
       },
     }
   },
+
   pickers = {
   },
+
   extensions = {
     fzf = {
-      fuzzy = true,                    -- false will only do exact matching
-      override_generic_sorter = true,  -- override the generic sorter
-      override_file_sorter = true,     -- override the file sorter
-      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-                                       -- the default case_mode is "smart_case"
+      fuzzy = true,
+      override_generic_sorter = true,
+      override_file_sorter = true,
+      case_mode = 'smart_case',
     }
   }
 }
