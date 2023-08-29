a1667a6c72cc82189c38d96eb9cb245efc59f921
Thomas Larrieu <thomas.larrieu@gmail.com>
2023-08-29 12:12:06 +0200

    Tweak neorg ToC behavior

--- nvim/lua/plugins/neorg.lua
+++ nvim/lua/plugins/neorg.lua
@@ -4,14 +4,19 @@ return {
   build = ":Neorg sync-parsers",
   ft = { 'norg' },
   keys = {
-    { '<leader>ei', '<cmd>Neorg index<cr>', desc = 'Neorg index' },
+    { '<leader>ei', '<cmd>Neorg index<cr>',    desc = 'Neorg index' },
     { '<leader>eq', '<cmd>Neorg return<cr>',   desc = 'Leave neorg' },
-    { '<leader>et', '<cmd>Neorg toc<cr>',   desc = 'Neorg ToC' },
+    { '<leader>et', '<cmd>Neorg toc left<cr>', desc = 'Neorg ToC' },
   },
   opts = {
     load = {
       ["core.defaults"] = {},
       ["core.concealer"] = {},
+      ["core.qol.toc"] = {
+        config = {
+          close_after_use = true
+        }
+      },
       ["core.dirman"] = {
         config = {
           workspaces = {
