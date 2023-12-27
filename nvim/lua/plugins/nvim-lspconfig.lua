6f6789fea614886874d0c8b96395ab4d5e91102d
Thomas Larrieu <thomas.larrieu@gmail.com>
2023-12-27 16:30:09 +0100

    Tweak mapping

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -9,7 +9,7 @@ return {
         local opts = { buffer = ev.buf }
         vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
         vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-        vim.keymap.set('n', 'gR', vim.lsp.buf.rename, opts)
+        vim.keymap.set('n', 'gé', vim.lsp.buf.rename, opts)
         vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
         vim.keymap.set('n', '<space>f', function()
           vim.lsp.buf.format { async = true }
