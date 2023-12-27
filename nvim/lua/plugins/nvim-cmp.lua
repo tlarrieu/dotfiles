cfa5fb8058ec189cff101708cf0b9441b463af9f
Thomas Larrieu <thomas.larrieu@gmail.com>
2023-12-27 18:18:39 +0100

    Do not display source in completion menu

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -4,31 +4,31 @@ return {
     local cmp = require('cmp')
 
     local kind_icons = {
-      Text = '',
-      Method = '󰆧',
-      Function = '󰊕',
-      Constructor = '',
-      Field = '󰇽',
-      Variable = '󰂡',
-      Class = '󰠱',
-      Interface = '',
-      Module = '',
-      Property = '󰜢',
-      Unit = '',
-      Value = '󰎠',
-      Enum = '',
-      Keyword = '󰌋',
-      Snippet = '',
-      Color = '󰏘',
-      File = '󰈙',
-      Reference = '',
-      Folder = '󰉋',
-      EnumMember = '',
-      Constant = '󰏿',
-      Struct = '',
-      Event = '',
-      Operator = '󰆕',
-      TypeParameter = '󰅲',
+      Text = '󰦨 ',
+      Method = '󰆧 ',
+      Function = '󰡱 ',
+      Constructor = ' ',
+      Field = '󰓽 ',
+      Variable = '󰫧 ',
+      Class = ' ',
+      Interface = ' ',
+      Module = ' ',
+      Property = '󰓽 ',
+      Unit = '󰍘 ',
+      Value = '󰎠 ',
+      Enum = '󰖽 ',
+      Keyword = ' ',
+      Snippet = '󰘦 ',
+      Color = ' ',
+      File = ' ',
+      Reference = ' ',
+      Folder = ' ',
+      EnumMember = '󰖽 ',
+      Constant = ' ',
+      Struct = ' ',
+      Event = '󱐌 ',
+      Operator = '󰿈 ',
+      TypeParameter = '󰅲 ',
     }
 
     cmp.setup({
@@ -49,13 +49,14 @@ return {
       formatting = {
         format = function(entry, vim_item)
           vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind)
-          vim_item.menu = ({
-            buffer = '[Buffer]',
-            nvim_lsp = '[LSP]',
-            luasnip = '[LuaSnip]',
-            nvim_lua = '[Lua]',
-            latex_symbols = '[LaTeX]',
-          })[entry.source.name]
+          -- vim_item.menu = ({
+          --   buffer = '[buff]',
+          --   nvim_lsp = '[LSP]',
+          --   luasnip = '[snip]',
+          --   ultisnips = '[snip]',
+          --   nvim_lua = '[Lua]',
+          --   latex_symbols = '[TeX]',
+          -- })[entry.source.name]
           return vim_item
         end
       }
