ffc78a16fcdb903c12d062fd9d5bfedfa6ee4f10
Thomas Larrieu <thomas.larrieu@gmail.com>
2023-12-29 15:09:53 +0100

    Tweak go mappings

--- nvim/ftplugin/go.lua
+++ nvim/ftplugin/go.lua
@@ -2,7 +2,8 @@ vim.opt_local.conceallevel = 2
 vim.opt_local.concealcursor = 'cni'
 vim.opt_local.expandtab = false
 
-vim.keymap.set('n', '<return>', ":T go run .<cr>", { silent = true, buffer = true })
+vim.keymap.set('n', '<return>', ":GoRun<cr>", { silent = true, buffer = true })
+vim.keymap.set('n', '<leader><return>', ":T go run .<cr>:Topen<cr>", { silent = true, buffer = true })
 
 local group = vim.api.nvim_create_augroup('GO_AUTOCMD', {})
 vim.api.nvim_create_autocmd('BufWritePre', {
