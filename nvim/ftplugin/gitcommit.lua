7353386390c11a7160db3e67ae6579c05875b7de
Thomas Larrieu <thomas.larrieu@gmail.com>
2024-01-02 17:55:21 +0100

    Remove useless config line

--- nvim/ftplugin/gitcommit.lua
+++ nvim/ftplugin/gitcommit.lua
@@ -3,8 +3,6 @@ local group = vim.api.nvim_create_augroup('GIT_AUTOCMD', {})
 vim.api.nvim_create_autocmd('BufEnter', {
   pattern = 'COMMIT_EDITMSG',
   callback = function()
-    -- move to end of first line
-    vim.api.nvim_win_set_cursor(0, { 1, 1000000 })
     -- enter insert mode
     vim.cmd('startinsert!')
     -- <c-s> saves and exits
