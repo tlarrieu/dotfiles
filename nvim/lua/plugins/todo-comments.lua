e563d3ae611763b47b900926b7c02b5d387527c5
Thomas Larrieu <thomas.larrieu@gmail.com>
2024-04-17 15:56:58 +0200

    Tweak todo-comments

--- nvim/lua/plugins/todo-comments.lua
+++ nvim/lua/plugins/todo-comments.lua
@@ -6,20 +6,19 @@ return {
   opts = {
     signs = false,   -- show icons in the signs column
     sign_priority = 8, -- sign priority
-    -- keywords recognized as todo comments
     keywords = {
       FIX = {
-        icon = ' ', -- icon used for the sign, and in search results
-        color = 'error', -- can be a hex color, or a named color (see below)
-        alt = { 'FIXME', 'BUG', 'FIXIT', 'ISSUE' }, -- a set of other keywords that all map to this FIX keywords
+        icon = ' ',
+        color = 'error',
+        alt = { 'FIXME', 'BUG', 'FIXIT', 'ISSUE' },
         -- signs = false, -- configure signs for some keywords individually
       },
-      TODO = { icon = ' ', color = 'info' },
-      HACK = { icon = ' ', color = 'warning' },
-      WARN = { icon = ' ', color = 'warning', alt = { 'WARNING', 'XXX' } },
-      PERF = { icon = ' ', alt = { 'OPTIM', 'PERFORMANCE', 'OPTIMIZE' } },
-      NOTE = { icon = ' ', color = 'hint', alt = { 'INFO' } },
-      TEST = { icon = '⏲ ', color = 'test', alt = { 'TESTING', 'PASSED', 'FAILED' } },
+      TODO = { icon = '󰥪 ', color = 'info' },
+      HACK = { icon = '󱍔 ', color = 'error' },
+      WARN = { icon = ' ', color = 'warning', alt = { 'WARNING', 'XXX' } },
+      PERF = { icon = '󰅒 ', alt = { 'OPTIM', 'PERFORMANCE', 'OPTIMIZE' } },
+      NOTE = { icon = ' ', color = 'hint', alt = { 'INFO' } },
+      TEST = { icon = '󰙨 ', color = 'test', alt = { 'TESTING' } },
     },
     gui_style = {
       fg = "NONE",       -- The gui style to use for the fg highlight group.
@@ -34,9 +33,9 @@ return {
       multiline = true,              -- enable multine todo comments
       multiline_pattern = "^.",      -- lua pattern to match the next multiline from the start of the matched keyword
       multiline_context = 10,        -- extra lines that will be re-evaluated when changing a line
-      before = "",                   -- "fg" or "bg" or empty
-      keyword = "wide",              -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
-      after = "fg",                  -- "fg" or "bg" or empty
+      before = '',                   -- "fg" or "bg" or empty
+      keyword = 'bg',                -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
+      after = 'fg',                  -- "fg" or "bg" or empty
       pattern = [[.*<(KEYWORDS)\s*:]], -- pattern or table of patterns, used for highlighting (vim regex)
       comments_only = true,          -- uses treesitter to match keywords in comments only
       max_line_len = 400,            -- ignore lines longer than this
@@ -64,7 +63,6 @@ return {
       -- regex that will be used to match keywords.
       -- don't replace the (KEYWORDS) placeholder
       pattern = [[\b(KEYWORDS):]], -- ripgrep regex
-      -- pattern = [[\b(KEYWORDS)\b]], -- match without the extra colon. You'll likely get false positives
     },
   }
 }
