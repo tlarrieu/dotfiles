e637d1d0b9d3e0aa41ba126cc5a6fcd038ed9beb
Thomas Larrieu <thomas.larrieu@gmail.com>
2024-04-20 01:50:29 +0200

    Tweak fold indicator symbols

--- nvim/lua/plugins/folds.lua
+++ nvim/lua/plugins/folds.lua
@@ -30,8 +30,8 @@ return {
     require('nvim-foldsign').setup({
       offset = -3,
       foldsigns = {
-        open = '-', -- mark the beginning of a fold
-        close = '+', -- show a closed fold
+        open = '―', -- mark the beginning of a fold
+        close = '', -- show a closed fold
         seps = { ' ' }, -- open fold middle marker
       }
     })
