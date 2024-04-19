29cdcdd9c032c2c0ab8851971b99d61f57de6014
Thomas Larrieu <thomas.larrieu@gmail.com>
2024-04-19 11:53:36 +0200

    Add convenience keymap

--- nvim/ftplugin/norg.lua
+++ nvim/ftplugin/norg.lua
@@ -4,3 +4,12 @@ o.textwidth = 130
 o.formatoptions = o.formatoptions + 't'
 o.conceallevel = 2
 o.spell = true
+
+local group = vim.api.nvim_create_augroup('NeorgAutocmd', {})
+vim.api.nvim_create_autocmd('BufEnter', {
+  pattern = { '**/gtd/index.norg' },
+  callback = function()
+    vim.keymap.set('n', 'o', 'i<c-cr>', { remap = true })
+  end,
+  group = group
+})
