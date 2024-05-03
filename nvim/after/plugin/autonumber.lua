9883e353868bfba8c626a6dd80dc2e1b724985ce
Thomas Larrieu <thomas.larrieu@gmail.com>
2024-05-03 23:25:05 +0200

    Auto toggle relative numbers (only displayed in the current window)

---
+++ nvim/after/plugin/autonumber.lua
@@ -0,0 +1,30 @@
+local group = vim.api.nvim_create_augroup('autonumber_group', {})
+
+vim.api.nvim_create_autocmd('WinEnter', {
+  pattern = { '*' },
+  callback = function()
+    if vim.wo.number then
+      vim.wo.relativenumber = true
+    end
+  end,
+  group = group
+})
+
+vim.api.nvim_create_autocmd('WinLeave', {
+  pattern = { '*' },
+  callback = function()
+    if vim.wo.number then
+      vim.wo.relativenumber = false
+    end
+  end,
+  group = group
+})
+
+-- auto turn on relativenumber if we activate number
+vim.api.nvim_create_autocmd('OptionSet', {
+  pattern = { 'number' },
+  callback = function()
+    vim.wo.relativenumber = vim.wo.number
+  end,
+  group = group
+})
