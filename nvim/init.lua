27b71957dfca818fff03660dcb8d15e3284b8c53
Thomas Larrieu <thomas.larrieu@gmail.com>
2024-05-06 17:53:30 +0200

    Fix quoting

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,17 +3,17 @@
 -- Designed for dvorak-bépo keyboard
 -- -----------------------------------------------------------------------------
 
-vim.g.mapleader = " "
+vim.g.mapleader = ' '
 
 -- get lazy.nvim if not present
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable',
     lazypath,
   })
 end
