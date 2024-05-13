e1982bf952e59e80171d699a1c23a6d3f5062161
Thomas Larrieu <thomas.larrieu@gmail.com>
2024-05-14 01:42:47 +0200

    Fix startinstert! when editing git commits

--- nvim/after/ftplugin/gitcommit.lua
+++ nvim/after/ftplugin/gitcommit.lua
@@ -3,7 +3,11 @@ local group = vim.api.nvim_create_augroup('GIT_AUTOCMD', {})
 vim.api.nvim_create_autocmd('BufEnter', {
   pattern = 'COMMIT_EDITMSG',
   callback = function()
-    vim.cmd('startinsert!')
+    vim.cmd.startinsert()
+    -- FIXME: since using neovim-git, startinsert! seems to be behaving weirdly
+    -- (and cursor gets lost in the comments, and not at the end of the first
+    -- line)
+    vim.api.nvim_win_set_cursor(0, {1, 300})
     vim.keymap.set( { 'n', 'i' }, '<c-s>', vim.cmd.x, { silent = true, buffer = 0 })
   end,
   group = group
