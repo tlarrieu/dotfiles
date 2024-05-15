2d13ebe807f4deceb21c47fc617b41604af82e13
Thomas Larrieu <thomas.larrieu@gmail.com>
2024-05-15 16:38:36 +0200

    Remove TODO comment

--- nvim/after/ftplugin/lua.lua
+++ nvim/after/ftplugin/lua.lua
@@ -2,8 +2,6 @@ local runner = require('runner')
 
 runner.default({ main = runner.term('lua %') })
 
--- TODO: make this more robust (if we are too “deep”, pattern does not get
--- triggered)
 runner.match({ 'config/awesome/**/*.lua', 'config/awesome/*.lua' }, {
   main = runner.term('~/scripts/awesome-test')
 })
