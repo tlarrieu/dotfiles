3cd24ef7e28f3ae1aeb550aa55550da2b886662d
Thomas Larrieu <thomas.larrieu@gmail.com>
2024-05-15 17:14:03 +0200

    Fix FIXME

--- nvim/after/ftplugin/gitcommit.lua
+++ nvim/after/ftplugin/gitcommit.lua
@@ -3,11 +3,7 @@ local group = vim.api.nvim_create_augroup('GIT_AUTOCMD', {})
 vim.api.nvim_create_autocmd('BufEnter', {
   pattern = 'COMMIT_EDITMSG',
   callback = function()
-    vim.cmd.startinsert()
-    -- FIXME: since using neovim-git, startinsert! seems to be behaving weirdly
-    -- (and cursor gets lost in the comments, and not at the end of the first
-    -- line)
-    vim.api.nvim_win_set_cursor(0, {1, 300})
+    vim.cmd.startinsert({ bang = true })
     vim.keymap.set( { 'n', 'i' }, '<c-s>', vim.cmd.x, { silent = true, buffer = 0 })
   end,
   group = group
