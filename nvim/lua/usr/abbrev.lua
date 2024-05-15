ef0758b9d07a569c0f7a530379b265e6aeaba9db
Thomas Larrieu <thomas.larrieu@gmail.com>
2024-05-15 17:11:54 +0200

    Make Map command deal with ' gracefully

--- nvim/lua/usr/abbrev.lua
+++ nvim/lua/usr/abbrev.lua
@@ -9,12 +9,11 @@ local abbr = {
   hi = "lua require('telescope.builtin').highlights()",
   lazy = 'Lazy',
   mason = 'Mason',
-  map = "Map", -- TODO: make this deal properly with '
+  map = "Map",
 }
 
 vim.api.nvim_create_user_command('Map', function(opts)
   local window_name = 'mappings'
-  -- clear previous buffer if it exists
   pcall(vim.cmd.bd, { window_name, bang = true })
   vim.cmd.vnew(window_name)
 
@@ -24,10 +23,15 @@ vim.api.nvim_create_user_command('Map', function(opts)
 
   vim.keymap.set('n', 'q', ':quit<cr>', { buffer = true, silent = true })
 
-  vim.cmd("silent put=execute('map " .. opts.args .. "')")
-  vim.cmd("silent put=execute('imap " .. opts.args .. "')")
-  vim.cmd("g/^$/d_")
-  vim.cmd("g/No mapping found/d_")
+  local lines = {}
+  local maps = vim.api.nvim_exec2('map ' .. opts.args, { output = true }).output
+  local imaps = vim.api.nvim_exec2('imap ' .. opts.args, { output = true }).output
+  for s in (maps .. imaps):gmatch("[^\r\n]+") do
+    table.insert(lines, s)
+  end
+  vim.api.nvim_buf_set_lines(0, -1, -1, true, lines)
+  vim.cmd("g/^$/d_", { silent = true })
+  vim.cmd("g/No mapping found/d_", { silent = true })
 
   vim.api.nvim_win_set_cursor(0, { 1, 0 })
 end, { nargs = 1, desc = 'show map result in a split' })
