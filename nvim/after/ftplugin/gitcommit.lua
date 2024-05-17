9e332d7cf6cb898dfab468cceaaf63185491c316
Thomas Larrieu <thomas.larrieu@gmail.com>
2024-05-17 20:24:21 +0200

    Update gitcommit autocmds

--- nvim/after/ftplugin/gitcommit.lua
+++ nvim/after/ftplugin/gitcommit.lua
@@ -1,21 +1,14 @@
-local group = vim.api.nvim_create_augroup('GIT_AUTOCMD', {})
-
 vim.api.nvim_create_autocmd('BufEnter', {
   pattern = 'COMMIT_EDITMSG',
   callback = function()
-    vim.cmd.startinsert({ bang = true })
+    local first_line = vim.api.nvim_buf_get_lines(0, 0, 1, false)[1]
+    print(vim.inspect(first_line))
+    if first_line == '' then
+      vim.cmd.startinsert({ bang = true })
+    end
     vim.keymap.set( { 'n', 'i' }, '<c-s>', vim.cmd.x, { silent = true, buffer = 0 })
   end,
-  group = group
-})
-
-vim.api.nvim_create_autocmd('BufEnter', {
-  pattern = 'index',
-  callback = function()
-    vim.keymap.set('n', '<c-s>', '<cmd>Gitcommit<cr>', { silent = true, buffer = 0 })
-    vim.keymap.set('n', '<c-a>', '<cmd>Gitcommit --amend<cr>', { silent = true, buffer = 0 })
-  end,
-  group = group
+  group = vim.api.nvim_create_augroup('GIT_AUTOCMD', {})
 })
 
 require('utils').autocapitalize('COMMIT_EDITMSG')
