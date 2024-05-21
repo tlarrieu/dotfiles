d963ce9aca7c22f3dcfe2caa9607637501710243
Thomas Larrieu <thomas.larrieu@gmail.com>
2024-05-21 14:09:49 +0200

    Fix quotes and remove comments

--- nvim/lua/plugins/todo-comments.lua
+++ nvim/lua/plugins/todo-comments.lua
@@ -4,8 +4,8 @@ return {
     { 'nvim-lua/plenary.nvim' }
   },
   opts = {
-    signs = false,   -- show icons in the signs column
-    sign_priority = 8, -- sign priority
+    signs = false,
+    sign_priority = 8,
     keywords = {
       FIX = {
         icon = '󰨰 ',
@@ -21,45 +21,39 @@ return {
       TEST = { icon = '󰙨 ', color = 'test', alt = { 'TESTING' } },
     },
     gui_style = {
-      fg = "NONE",       -- The gui style to use for the fg highlight group.
-      bg = "BOLD",       -- The gui style to use for the bg highlight group.
+      fg = 'NONE',
+      bg = 'BOLD',
     },
-    merge_keywords = true, -- when true, custom keywords will be merged with the defaults
-    -- highlighting of the line containing the todo comment
-    -- * before: highlights before the keyword (typically comment characters)
-    -- * keyword: highlights of the keyword
-    -- * after: highlights after the keyword (todo text)
+    merge_keywords = true,
     highlight = {
-      multiline = true,              -- enable multine todo comments
-      multiline_pattern = "^.",      -- lua pattern to match the next multiline from the start of the matched keyword
-      multiline_context = 10,        -- extra lines that will be re-evaluated when changing a line
-      before = '',                   -- "fg" or "bg" or empty
-      keyword = 'bg',                -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
-      after = 'fg',                  -- "fg" or "bg" or empty
-      pattern = [[.*<(KEYWORDS)(\(.*\))?\s*:]], -- pattern or table of patterns, used for highlighting (vim regex)
-      comments_only = true,          -- uses treesitter to match keywords in comments only
-      max_line_len = 400,            -- ignore lines longer than this
-      exclude = {},                  -- list of file types to exclude highlighting
+      multiline = true,
+      multiline_pattern = '^.',
+      multiline_context = 10,
+      before = '',
+      keyword = 'bg',
+      after = 'fg',
+      pattern = [[.*<(KEYWORDS)(\(.*\))?\s*:]],
+      comments_only = true,
+      max_line_len = 400,
+      exclude = {},
     },
-    -- list of named colors where we try to extract the guifg from the
-    -- list of highlight groups or use the hex color if hl not found as a fallback
     colors = {
-      error = { "DiagnosticError", "ErrorMsg", "#DC2626" },
-      warning = { "DiagnosticWarn", "WarningMsg", "#FBBF24" },
-      info = { "DiagnosticInfo", "#2563EB" },
-      hint = { "DiagnosticHint", "#10B981" },
-      default = { "Identifier", "#7C3AED" },
-      test = { "Identifier", "#FF00FF" }
+      error = { 'DiagnosticError', 'ErrorMsg', '#DC2626' },
+      warning = { 'DiagnosticWarn', 'WarningMsg', '#FBBF24' },
+      info = { 'DiagnosticInfo', '#2563EB' },
+      hint = { 'DiagnosticHint', '#10B981' },
+      test = { 'Identifier', '#FF00FF' },
+      default = { 'Identifier', '#7C3AED' },
     },
     search = {
-      command = "rg",
+      command = 'rg',
       args = {
-        "--color=never",
-        "--no-heading",
-        "--with-filename",
-        "--line-number",
-        "--column",
-        "--iglob=!**/snippets/all.lua",
+        '--color=never',
+        '--no-heading',
+        '--with-filename',
+        '--line-number',
+        '--column',
+        '--iglob=!**/snippets/all.lua',
       },
       -- regex that will be used to match keywords.
       -- don't replace the (KEYWORDS) placeholder
