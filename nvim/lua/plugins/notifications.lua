6c708865ff19ade88d7480a3126ec725ef41509d
Thomas Larrieu <thomas.larrieu@gmail.com>
2024-05-27 20:24:36 +0200

    Remove obsolete binding
    We mostly set it up before plugins caught up to nvim 0.11 changes.

--- nvim/lua/plugins/notifications.lua
+++ nvim/lua/plugins/notifications.lua
@@ -103,6 +103,5 @@ return {
         path = string.format('%s/fidget.nvim.log', vim.fn.stdpath('cache')),
       },
     })
-    vim.keymap.set('n', '<leader>h', require('fidget.notification').clear)
   end
 }
