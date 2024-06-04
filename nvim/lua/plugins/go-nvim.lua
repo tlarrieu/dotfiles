0430b57d26a817bd2ec1071cb24052cff6b5fc07
Thomas Larrieu <thomas.larrieu@gmail.com>
2024-06-05 00:14:56 +0200

    Disable signs for diagnostics
    We dont want the gutter to jump around when errors are showing up

--- nvim/lua/plugins/go-nvim.lua
+++ nvim/lua/plugins/go-nvim.lua
@@ -5,10 +5,10 @@ return {
     'neovim/nvim-lspconfig',
     'nvim-treesitter/nvim-treesitter',
   },
-  config = function()
-    require('go').setup()
-  end,
-  event = { 'CmdlineEnter' },
   ft = { 'go', 'gomod' },
-  build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+  build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+  diagnostic = false,
+  config = function(_, opts)
+    require('go').setup(opts)
+  end,
 }
