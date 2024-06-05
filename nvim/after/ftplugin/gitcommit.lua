cd2989d2bb984bfc029377f4329c445e86f15a9c
Thomas Larrieu <thomas.larrieu@gmail.com>
2024-06-05 11:19:30 +0200

    Fix cursor position in new gitcommit files
    For some reason, cursor is not at the beginning of the buffer anymore.
    I dont know what has changed…

--- nvim/after/ftplugin/gitcommit.lua
+++ nvim/after/ftplugin/gitcommit.lua
@@ -2,8 +2,8 @@ vim.api.nvim_create_autocmd('BufEnter', {
   pattern = 'COMMIT_EDITMSG',
   callback = function()
     local first_line = vim.api.nvim_buf_get_lines(0, 0, 1, false)[1]
-    print(vim.inspect(first_line))
     if first_line == '' then
+      vim.cmd.normal('gg')
       vim.cmd.startinsert({ bang = true })
     end
     vim.keymap.set( { 'n', 'i' }, '<c-s>', vim.cmd.x, { silent = true, buffer = 0 })
