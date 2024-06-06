10d855368d412474372928c5064b3099e1cc9b3d
Thomas Larrieu <thomas.larrieu@gmail.com>
2024-06-06 17:11:45 +0200

    Remove conceal related configuration for markdown files

--- nvim/after/ftplugin/markdown.lua
+++ nvim/after/ftplugin/markdown.lua
@@ -5,16 +5,6 @@ o.formatoptions = o.formatoptions + 't'
 o.foldlevel = 1
 o.foldlevelstart = 10
 o.spell = true
-o.concealcursor = 'ncv'
-o.conceallevel = 2
-
-vim.keymap.set('n', '<leader>i', function()
-  if vim.opt.conceallevel:get() == 2 then
-    o.conceallevel = 0
-  else
-    o.conceallevel = 2
-  end
-end, { silent = true, buffer = true })
 
 local runner = require('runner')
 runner.default({ main = runner.term("mdprev w '%'") })
@@ -26,9 +16,3 @@ vim.keymap.set('v', '<leader>s', 'S~gvS~eee', { buffer = true, remap = true })
 require('utils').autocapitalize('*.md')
 
 local group = vim.api.nvim_create_augroup('MARKDOWN_AUTOCMD', {})
-
-vim.api.nvim_create_autocmd('InsertLeave', {
-  pattern = { '*' },
-  callback = function() o.conceallevel = 2 end,
-  group = group
-})
