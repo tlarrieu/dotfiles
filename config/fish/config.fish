a5c0e4ecd67e441cc15e742677705e9a1ed686a8
Thomas Larrieu <thomas.larrieu@gmail.com>
2024-06-07 21:07:33 +0200

    Update config to make fzf play well with both dark and light theme

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,28 +3,17 @@ stty -ixon 2> /dev/null
 bind \cf edit_command_buffer
 
 # FZF
-set -l color00 '#fdf6e3'
-set -l color01 '#eee8d5'
-set -l color02 '#93a1a1'
-set -l color03 '#839496'
-set -l color04 '#657b83'
-set -l color05 '#586e75'
-set -l color06 '#073642'
-set -l color07 '#002b36'
-set -l color08 '#dc322f'
-set -l color09 '#cb4b16'
-set -l color0A '#b58900'
-set -l color0B '#859900'
-set -l color0C '#2aa198'
-set -l color0D '#268bd2'
-set -l color0E '#6c71c4'
-set -l color0F '#d33682'
+set -l fzf_main_color '#268bd2' # blue
+set -l fzf_secondary_color '#d33682' # magenta
 
 set -l fzf_common_opts "
   --reverse
-  --color=bg+:$color01,bg:$color00,spinner:$color0C,hl:$color0D
-  --color=fg:$color04,header:$color0D,info:$color0A,pointer:$color0C
-  --color=marker:$color0C,fg+:$color06,prompt:$color0A,hl+:$color0D
+  --color=bw
+  --color=query:$fzf_main_color:regular
+  --color=fg+:$fzf_main_color:regular
+  --color=pointer:$fzf_main_color:regular
+  --color=hl:$fzf_secondary_color:underline
+  --color=hl+:$fzf_secondary_color:underline
   --bind=ctrl-k:kill-line
   --border=none
   --no-scrollbar
