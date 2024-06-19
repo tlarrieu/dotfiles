c248adcea55e9c18a9db760a8f7afe379a357568
Thomas Larrieu <thomas.larrieu@gmail.com>
2024-06-20 01:17:05 +0200

    Tweak push command in fugitive window
    We now --force-with-lease

--- nvim/after/ftplugin/fugitive.lua
+++ nvim/after/ftplugin/fugitive.lua
@@ -1,5 +1,5 @@
 local runner = require('runner')
-runner.default({ alt = runner.exec('Git push<bar>Git') })
+runner.default({ alt = runner.exec('Git push --force-with-lease<bar>Git') })
 
 vim.keymap.set('n', 'k', '(', { remap = true, buffer = true })
 vim.keymap.set('n', 'j', ')', { remap = true, buffer = true })
