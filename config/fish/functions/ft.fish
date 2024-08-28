e9bb34e6ec8d3becebc24513ac60c9a19359b2be
Thomas Larrieu <thomas.larrieu@gmail.com>
2024-08-28 11:37:09 +0200

    Move ft function to a dedicated file

---
+++ config/fish/functions/ft.fish
@@ -0,0 +1,46 @@
+function ft
+  set -l flags --strict
+  switch $argv[1]
+  case edit
+    nvim -p (readlink -f ~/.hledger/current.journal)
+    return
+  case bal bs bse is cf roi
+    set flags $flags --pretty --auto
+    if [ $argv[1] != roi ]
+      set flags $flags --layout=bare
+    end
+    if [ $argv[1] = bse ]
+      # resolve accounting equation
+      set flags $flags --alias '/^(income|expenses)/=equity:\1'
+    end
+  case now
+    ft bal --empty -p today -H type:C not:tag:miriam
+    ft bal --empty -p thismonth expenses:groceries not:tag:miriam
+    ft bal --empty -p thisyear expenses:clothing expenses:gifts not:tag:miriam
+    return
+  case up upcoming
+    ft areg assets:check:ce tag:generated-transaction 'expenses|loan' --fore=today.. -p thismonth
+    return
+  case bud budget
+    ft bal --budget -p thismonth not:tag:miriam --empty
+    return
+  case we weeks
+    ft bs --fore=tomorrow.. -W -p today..30days not:tag:miriam
+    return
+  case mo months
+    ft bs --fore=tomorrow.. -M -p 1..6months not:tag:miriam
+    return
+  case 2024
+    ft bs --fore=tomorrow.. -M -b 2024/06 -e 2025 --color=always | less -RS
+    return
+  case 2025
+    ft bs --fore=tomorrow.. -M -p 2025 --color=always | less -RS
+    return
+  end
+
+  echo -n -e "\e[1;38m"
+  echo hledger $argv $flags
+  echo -n -e "\e[0m"
+
+  hledger $argv 1> /dev/null; and hledger -f ~/.hledger.journal $argv $flags
+end
