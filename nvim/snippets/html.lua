03226df5c8ab92dfcb452c28d66723b1c8f1616e
Thomas Larrieu <thomas.larrieu@gmail.com>
2024-09-03 17:34:36 +0200

    Update snippets

--- nvim/snippets/html.lua
+++ nvim/snippets/html.lua
@@ -18,14 +18,15 @@ return {
 
   s("meta", fmt('<meta {}="{}" />', { i(1, 'key'), i(0, 'value') })),
   s("title", fmt('<title>{}</title>', { i(0, 'title') })),
-  s("h1", fmt('<h1>{}</h1>', { i(0, 'title') })),
-  s("h2", fmt('<h2>{}</h2>', { i(0, 'title') })),
-  s("h3", fmt('<h3>{}</h3>', { i(0, 'title') })),
-  s("h4", fmt('<h4>{}</h4>', { i(0, 'title') })),
-  s("h5", fmt('<h5>{}</h5>', { i(0, 'title') })),
-  s("h6", fmt('<h6>{}</h6>', { i(0, 'title') })),
-  s("div", fmt('<div>{}</div>', { i(0, 'content') })),
-  s("span", fmt('<span>{}</span>', { i(0, 'content') })),
+  s("h1", fmt('<h1>{}{}</h1>', { sel(), i(0) })),
+  s("h2", fmt('<h2>{}{}</h2>', { sel(), i(0) })),
+  s("h3", fmt('<h3>{}{}</h3>', { sel(), i(0) })),
+  s("h4", fmt('<h4>{}{}</h4>', { sel(), i(0) })),
+  s("h5", fmt('<h5>{}{}</h5>', { sel(), i(0) })),
+  s("h6", fmt('<h6>{}{}</h6>', { sel(), i(0) })),
+  s("p", fmt('<p>{}{}</p>', { sel(), i(0) })),
+  s("div", fmt('<div>{}{}</div>', { sel(), i(0) })),
+  s("span", fmt('<span>{}{}</span>', { sel(), i(0) })),
   s("img", fmt('<img src="{}" />', { i(0, 'src') })),
 }, {
 }
