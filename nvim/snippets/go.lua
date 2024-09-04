47cd1e143f280679dd5f1c66de16d9853489d1b4
Thomas Larrieu <thomas.larrieu@gmail.com>
2024-09-04 17:20:59 +0200

    Tweak go snippets

--- nvim/snippets/go.lua
+++ nvim/snippets/go.lua
@@ -22,6 +22,13 @@ return {
     { i(1, "name"), i(2, "arg"), i(3, "type"), i(0) }
   )),
   s("d", fmta([[ func (<>) <> {<>}]], { i(1, "arg"), i(2, "type"), i(0) })),
+  rs("^dr", fmta([[
+      func <>(writer http.ResponseWriter, request *http.Request) {
+      	<>
+      }]],
+    { i(1, "name"), i(0) }
+  )),
+  s("dr", fmta([[ func (writer http.ResponseWriter, request *http.Request) {<>}]], { i(0) })),
 
   -- types
   rs("^i", fmta("type <> interface {<>}", { i(1, "name"), i(0) })),
