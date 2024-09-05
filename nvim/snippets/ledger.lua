21879a6d9d74f182afb0a3b485e21ad5405a7364
Thomas Larrieu <thomas.larrieu@gmail.com>
2024-09-05 12:27:22 +0200

    Update ledger snippets

--- nvim/snippets/ledger.lua
+++ nvim/snippets/ledger.lua
@@ -3,6 +3,7 @@
 local curdate = function () return os.date('%m/%d') end
 
 return {
+  -- definitions
   s("e", fmta([[
   <> ! <> | <>
     <>  <> €
@@ -34,13 +35,22 @@ return {
     i(2, 'amount'),
     i(0, 'from')
   })),
+  s("l", fmta("<>  <> €", {
+    i(1, 'account'),
+    i(0, 'amount')
+  })),
+
+  -- Specific expenses
   s("m", fmta([[
   <> * caisse d'épargne | échéance crédit
     card:ce  -1403.16 €
     mortgage  1051.63 €
     bank:loans
+  <> * caisse d'épargne | assurance crédit
+    bank:fees  3.05 €
+    card:ce
   ]], {
-    f(curdate),
+    f(curdate), f(curdate),
   })),
   s("q", fmta([[
   <> * église | quête
