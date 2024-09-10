468e27be42751686eb7f0dd3b17355abd9d0745d
Thomas Larrieu <thomas.larrieu@gmail.com>
2024-09-11 01:13:08 +0200

    Create JPGs instead of PNGs when taking screenshots
    I'm not sure why we were using PNGs in the first place

--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-filename="$HOME/Pictures/screenshots/$(date +'%Y%m%d%H%M%S').png"
+filename="$HOME/Pictures/screenshots/$(date +'%Y%m%d%H%M%S').jpg"
 maim -s | feh - --class screenshot --action "mv %F $filename"
