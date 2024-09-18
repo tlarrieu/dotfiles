9c56851c736be8ef3be2a4f5a8169941b2395226
Thomas Larrieu <thomas.larrieu@gmail.com>
2024-09-18 03:46:42 +0200

    Update plugins

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -3,7 +3,6 @@ return {
   branch = "harpoon2",
   dependencies = {
     'nvim-lua/plenary.nvim',
-    'nvim-telescope/telescope.nvim',
   },
   config = function()
     local harpoon = require('harpoon')
