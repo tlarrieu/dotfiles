1c8050521d7ab5127524096a0842728f3ebfb6e6
Thomas Larrieu <thomas.larrieu@gmail.com>
2024-09-19 23:57:58 +0200

    Add support for -n flag in watch command

--- config/fish/functions/watch.fish
+++ config/fish/functions/watch.fish
@@ -1,3 +1,10 @@
 function watch
-  command watch --color --exec fish -c "$argv"
+  argparse -i n/interval= -- $argv
+
+  set -l flags
+  if set -ql _flag_n
+    set flags "-n" $_flag_interval
+  end
+
+  command watch $flags --color --exec fish -c "$argv"
 end
