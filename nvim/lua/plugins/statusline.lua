6443661be6679c0ee7b33211cf4bf288a2900ffd
Thomas Larrieu <thomas.larrieu@gmail.com>
2024-09-25 01:49:54 +0200

    Add commented inverted separators for quick reference

--- nvim/lua/plugins/statusline.lua
+++ nvim/lua/plugins/statusline.lua
@@ -15,6 +15,10 @@ return {
       component_separators = { left = '', right = '' },
       section_separators = { left = '', right = '' },
     },
+    -- options = {
+    --   component_separators = { left = '', right = '' },
+    --   section_separators = { left = '', right = '' },
+    -- },
     sections = {
       lualine_b = {
         { 'branch' },
