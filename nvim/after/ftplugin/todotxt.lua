e9d144562428f774ba5111b27aef1af698736a29
Thomas Larrieu <thomas.larrieu@gmail.com>
2024-09-24 04:52:06 +0200

    Add a link to format rules for todo.txt

--- nvim/after/ftplugin/todotxt.lua
+++ nvim/after/ftplugin/todotxt.lua
@@ -1,3 +1,6 @@
+-- Format rules :
+-- https://github.com/todotxt/todo.txt?tab=readme-ov-file#todotxt-format-rules
+
 vim.opt_local.iskeyword = vim.opt_local.iskeyword + '@' + '-' + '+'
 vim.opt_local.textwidth = 0
 
