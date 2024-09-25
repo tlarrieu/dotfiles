6382a2016e87359905ae38d4c13e58c3229b21e4
Thomas Larrieu <thomas.larrieu@gmail.com>
2024-09-25 22:43:03 +0200

    Fix numbering in help files

--- nvim/after/ftplugin/help.lua
+++ nvim/after/ftplugin/help.lua
@@ -2,3 +2,4 @@ vim.keymap.set('n', '<cr>', '<c-]>', { buffer = true })
 vim.keymap.set('n', '<bs>', '<c-T>', { buffer = true })
 vim.opt_local.conceallevel = 0
 vim.opt_local.number = true
+vim.opt_local.relativenumber = true
