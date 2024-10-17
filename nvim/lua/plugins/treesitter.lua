0a825d494f0e3d2f7a8c7db148aa2de2b2ede09e
Thomas Larrieu <thomas.larrieu@gmail.com>
2024-10-17 22:10:39 +0200

    Disable JS/TS treesitter
    They are fucked up currently, can't figure out which version of
    treesitter/nvim it works with

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -31,7 +31,7 @@ return {
     auto_install = true,
     highlight = {
       enable = true,
-      disable = { 'embedded_template', 'html' },
+      disable = { 'embedded_template', 'html', 'javascript', 'typescript' },
       additional_vim_regex_highlighting = { 'make' },
     },
     indent = { enable = true, disable = { 'embedded_template', 'html' } },
@@ -39,6 +39,7 @@ return {
       enable = true,
       select = {
         enable = true,
+        disable = { 'javascript', 'typescript' },
         lookahead = true,
 
         keymaps = {
@@ -60,16 +61,5 @@ return {
   },
   config = function(_, opts)
     require('nvim-treesitter.configs').setup(opts)
-
-    -- vim.api.nvim_create_autocmd('Filetype', {
-    --   pattern = { '*' },
-    --   callback = function(args)
-    --     local ft = args.match
-    --     if vim.treesitter.language.get_lang(ft) then
-    --       vim.opt_local.spell = true
-    --     end
-    --   end,
-    --   group = vim.api.nvim_create_augroup('treesitter_autocmd', {})
-    -- })
   end
 }
