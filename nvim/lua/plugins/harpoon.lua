577875a0e2c28c11702f5c1542870f04a35f0f6b
Thomas Larrieu <thomas.larrieu@gmail.com>
2024-10-21 13:34:46 +0200

    Add description to harpoon keymap

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -7,22 +7,26 @@ return {
     'nvim-lua/plenary.nvim',
   },
   keys = {
-    { '<c-$>', function() list():add() end },
+    { '<c-$>', function() list():add() end,     desc = 'Harpoon: add file' },
 
-    { '<c-">', function() list():select(1) end },
-    { '<c-«>', function() list():select(2) end },
-    { '<c-»>', function() list():select(3) end },
-    { '<c-(>', function() list():select(4) end },
-    { '<c-)>', function() list():select(4) end },
+    { '<c-">', function() list():select(1) end, desc = 'Harpoon: open #1' },
+    { '<c-«>', function() list():select(2) end, desc = 'Harpoon: open #2' },
+    { '<c-»>', function() list():select(3) end, desc = 'Harpoon: open #3' },
+    { '<c-(>', function() list():select(4) end, desc = 'Harpoon: open #4' },
+    { '<c-)>', function() list():select(5) end, desc = 'Harpoon: open #5' },
 
-    { "<c-,>", function()
-      local harpoon = require('harpoon')
-      harpoon.ui:toggle_quick_menu(harpoon:list(), {
-        title = ' ⇁ Harpoon ',
-        title_pos = 'left',
-        border = 'single',
-      })
-    end },
+    {
+      "<c-,>",
+      function()
+        local harpoon = require('harpoon')
+        harpoon.ui:toggle_quick_menu(harpoon:list(), {
+          title = ' ⇁ Harpoon ',
+          title_pos = 'left',
+          border = 'single',
+        })
+      end,
+      desc = 'Harpoon: edit list'
+    },
   },
   config = true,
 }
