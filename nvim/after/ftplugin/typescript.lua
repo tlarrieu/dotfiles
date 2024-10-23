d6f99fc0a03344cacb619f7cc7a063ed5dca59ef
Thomas Larrieu <thomas.larrieu@gmail.com>
2024-10-23 12:07:34 +0200

    Extend the list of patterns for test files

--- nvim/after/ftplugin/typescript.lua
+++ nvim/after/ftplugin/typescript.lua
@@ -6,7 +6,7 @@ o.formatprg = 'prettier'
 
 local runner = require('runner')
 runner.default({ alt = runner.test.file() })
-runner.match('*.test.ts', {
-    main = runner.test.nearest(),
-    alt = runner.test.file()
+runner.match({ '*.test.ts', '*.test.tsx', '*.spec.ts', '*.spec.tsx' }, {
+  main = runner.test.nearest(),
+  alt = runner.test.file()
 })
