73418cb21c38d1ed5a48b46328e1c5428161cb4a
Thomas Larrieu <thomas.larrieu@gmail.com>
2024-10-24 21:24:32 +0200

    Remove useless keybinds
    We're not displaying tab names anymore

--- nvim/lua/plugins/statusline.lua
+++ nvim/lua/plugins/statusline.lua
@@ -2,10 +2,6 @@ return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   lazy = false,
-  keys = {
-    { '<leader>tl', ':LualineRename ' },
-    { '<leader>tr', ':LualineRename<cr>', { silent = true } },
-  },
   opts = {
     extensions = {
       'man',
