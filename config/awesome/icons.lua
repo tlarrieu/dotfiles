1d467739284d060addac54930d16f915257c7d89
Thomas Larrieu <thomas.larrieu@gmail.com>
2024-11-13 11:06:59 +0100

    Add an app for dalibo

--- config/awesome/icons.lua
+++ config/awesome/icons.lua
@@ -59,6 +59,7 @@ local config = {
   { icon = '', rule = { class = 'Chromium', instance = 'linear.app' } },
   { icon = '󰨰', rule = { class = 'Chromium', instance = 'sentry.io' } },
   { icon = '󱘶', rule = { class = 'Chromium', instance = 'metabase' } },
+  { icon = '󱤢', rule = { class = 'Chromium', instance = 'dalibo' } },
   {
     icon = '󰡴',
     rules = {
