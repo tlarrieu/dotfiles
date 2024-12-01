16ceafe3c9c89d5e69800166a0536f259d33c13b
Thomas Larrieu <thomas.larrieu@gmail.com>
2024-12-01 18:30:49 +0100

    Update ft

--- config/fish/functions/ft.fish
+++ config/fish/functions/ft.fish
@@ -20,8 +20,8 @@ function ft
     ft bal -p today -C -H type:C not:tag:miriam
     echo -e "\e[33mPending balance (all transactions)\e[0m"
     ft bal -p today..7days -H type:C not:tag:miriam
-    echo -e "\e[34mPending balance (business expenses)\e[0m"
-    ft bal tag:business type:RX --empty -p today -H
+    # echo -e "\e[34mPending balance (business expenses)\e[0m"
+    # ft bal tag:business type:RX --empty -p today -H
     echo -e "\e[35mMonthly envelopes\e[0m"
     ft bal --empty -p thismonth expenses:groceries expenses:restaurant expenses:leisure not:tag:miriam
     echo -e "\e[35mYearly envelopes\e[0m"
