cbd8d408554fe30e1af3e3cac733d5b3907a17d3
Thomas Larrieu <thomas.larrieu@gmail.com>
2024-12-05 14:33:48 +0100

    Fix surround insert mode mapping
    We don't want it!

--- nvim/lua/plugins/surrounds.lua
+++ nvim/lua/plugins/surrounds.lua
@@ -1,8 +1,8 @@
+vim.g.surround_no_insert_mappings = 1
+
 return {
   'tpope/vim-surround',
   config = function()
-    vim.g.surround_no_insert_mappings = 1
-
     vim.keymap.set({ 'n', 'o' }, 'S', 'ys', { remap = true, silent = true })
     vim.keymap.set({ 'x' }, 'S', '<Plug>VSurround', { remap = true, silent = true })
   end
