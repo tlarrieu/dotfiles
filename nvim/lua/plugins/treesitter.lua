f1983198b59048c902f252327ba9832cc678f554
Thomas Larrieu <thomas.larrieu@gmail.com>
2024-12-19 16:13:37 +0100

    Disable treesitter ruby indentation
    Pressing `.` sometimes behave in a weird way and fucks the indentation
    up, it's not helping…

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -34,7 +34,7 @@ return {
       enable = true,
       additional_vim_regex_highlighting = { 'make' },
     },
-    indent = { enable = true },
+    indent = { enable = true, disable = { 'ruby' } },
     textobjects = {
       enable = true,
       select = {
