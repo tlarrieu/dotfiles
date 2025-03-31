9cdbbed16999c31de3b79a5dc76e0b7944bde1cf
Thomas Larrieu <thomas.larrieu@gmail.com>
2025-03-31 09:38:39 +0200

    Use biome for javascript formatting

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -3,6 +3,10 @@ local format = function() require('conform').format() end
 return {
   'stevearc/conform.nvim',
   opts = {
+    formatters_by_ft = {
+      javascript = { 'biome' },
+      typescript = { 'biome' },
+    },
     default_format_opts = {
       lsp_format = 'prefer',
       async = true,
