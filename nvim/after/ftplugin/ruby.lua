1398cb644589022d93376354b886813acea625b0
Thomas Larrieu <thomas.larrieu@gmail.com>
2025-05-26 09:12:19 +0200

    Add jbuilder files to autoformat

--- nvim/after/ftplugin/ruby.lua
+++ nvim/after/ftplugin/ruby.lua
@@ -28,7 +28,7 @@ runner.match({ 'Gemfile', '*.gemspec' }, { main = runner.term('bundle') })
 
 runner.match('config/routes.rb', { main = runner.term('rails routes') })
 
-require('utils').autoformat('*.rb')
+require('utils').autoformat({ '*.rb', '*.json.jbuilder' })
 
 local alternate = function()
   local path = vim.api.nvim_buf_get_name(0)
