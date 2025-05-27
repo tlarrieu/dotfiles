1d0edd488718051a7ffcdb0f1af01830ae2ad214
Thomas Larrieu <thomas.larrieu@gmail.com>
2025-05-27 19:35:57 +0200

    Update ledger ftplugin

--- nvim/after/ftplugin/ledger.lua
+++ nvim/after/ftplugin/ledger.lua
@@ -1,6 +1,6 @@
 local o = vim.opt_local
 o.commentstring = '; %s'
-o.iskeyword = o.iskeyword + ':' + '/'
+o.iskeyword = o.iskeyword + ':' + '/' + '.'
 o.expandtab = true
 o.tabstop = 2
 o.shiftwidth = 2
