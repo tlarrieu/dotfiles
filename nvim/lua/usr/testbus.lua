bec114cceb55755832da328c18040e07a7eccbc7
Thomas Larrieu <thomas.larrieu@gmail.com>
2025-07-02 20:58:20 +0200

    Reorganize testbus a bit
    Prepare things for when we don't go through toggleterm anymore

--- nvim/lua/usr/testbus.lua
+++ nvim/lua/usr/testbus.lua
@@ -1,39 +1,45 @@
-local rspec = function(data)
-  local started = false
-  local success = false
-  local failure = false
-  local stopped = false
-  local progress
+local adapters = {
+  rspec = function(data)
+    local started = false
+    local success = false
+    local failure = false
+    local stopped = false
+    local progress
 
-  for _, line in ipairs(data) do
-    local match = line:match("%d+/%d+")
-    if match then progress = match end
+    for _, line in ipairs(data) do
+      local match = line:match("%d+/%d+")
+      if match then progress = match end
 
-    if line:find('rspec') then started = true end
-    if line:find('shutting down') then stopped = true end
-    if not stopped then
-      if line:find('0 failures') then
-        success = true
-      elseif line:find('failure') then
-        failure = true
+      if line:find('rspec') then started = true end
+      if line:find('shutting down') then stopped = true end
+      if not stopped then
+        if line:find('0 failures') then
+          success = true
+        elseif line:find('failure') then
+          failure = true
+        end
       end
     end
-  end
 
-  if not success and not failure and not stopped then
-    vim.g.test_progress = progress
-  end
+    if not success and not failure and not stopped then
+      vim.g.test_progress = progress
+    end
 
-  if success then
-    vim.g.test_status = 'success'
-  elseif failure then
-    vim.g.test_status = 'failure'
-  elseif started then
-    vim.g.test_status = 'running'
-  elseif stopped then
-    vim.g.test_status = 'stopped'
+    if success then
+      vim.g.test_status = 'success'
+    elseif failure then
+      vim.g.test_status = 'failure'
+    elseif started then
+      vim.g.test_status = 'running'
+    elseif stopped then
+      vim.g.test_status = 'stopped'
+    end
   end
-end
+}
+
+local handlers = {
+  ruby = adapters.rspec
+}
 
 return {
   lualine = {
@@ -57,10 +63,10 @@ return {
       if vim.g.test_status == 'failure' then return { fg = 167 } end -- red
     end,
   },
-  on_stdout = function(stdout)
-    rspec(stdout)
-  end,
-  on_exit = function()
-    if vim.g.test_status == 'running' then vim.g.test_status = 'stopped' end
+  parse = function(stdout) handlers.ruby(stdout) end,
+  interrupt = function()
+    if vim.g.test_status ~= 'running' then return end
+
+    vim.g.test_status = 'stopped'
   end
 }
