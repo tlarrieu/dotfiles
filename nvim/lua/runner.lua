3f09c4b44a8b185232813a3c2264a0730cc5b022
Thomas Larrieu <thomas.larrieu@gmail.com>
2025-07-10 20:50:42 +0200

    Small tweak

--- nvim/lua/runner.lua
+++ nvim/lua/runner.lua
@@ -1,5 +1,5 @@
 ---@alias Runner { default: function, term: function, match: function, test: { nearest: function, file: function, last: function } }
-local _M = {}
+local M = {}
 
 -- private
 
@@ -13,7 +13,7 @@ end
 
 -- public
 
-_M.term = function(cmd, opts)
+M.term = function(cmd, opts)
   opts = opts or {}
   return function()
     vim.cmd.TermExec({
@@ -30,25 +30,25 @@ local wrap = function(fun)
   return function() testbus.start(fun) end
 end
 
-_M.test = {
+M.test = {
   nearest = function() return wrap(vim.cmd.TestNearest) end,
   file = function() return wrap(vim.cmd.TestFile) end,
   last = function() return wrap(vim.cmd.TestLast) end,
 }
 
-_M.default = function(opts)
+M.default = function(opts)
   if opts.main then _main(opts.main) end
   if opts.alt then _alt(opts.alt) end
 end
 
-_M.match = function(pattern, opts)
+M.match = function(pattern, opts)
   local group = vim.api.nvim_create_augroup(vim.inspect(pattern) .. '_runner_autocmd', {})
 
   vim.api.nvim_create_autocmd('BufEnter', {
     pattern = pattern,
-    callback = function() _M.default(opts) end,
+    callback = function() M.default(opts) end,
     group = group,
   })
 end
 
-return _M
+return M
