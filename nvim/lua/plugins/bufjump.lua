46eca856a597dc885be91ae42c146b57586bd13a
Thomas Larrieu <thomas.larrieu@gmail.com>
2025-07-17 12:03:54 +0200

    Small tweaks

--- nvim/lua/plugins/bufjump.lua
+++ nvim/lua/plugins/bufjump.lua
@@ -3,7 +3,7 @@ return {
   config = function()
     local bufjump = require('bufjump')
     bufjump.setup()
-    vim.keymap.set("n", "<c-o>", bufjump.backward, { silent = true })
-    vim.keymap.set("n", "<c-i>", bufjump.forward, { silent = true })
+    vim.keymap.set('n', '<c-o>', bufjump.backward)
+    vim.keymap.set('n', '<c-i>', bufjump.forward)
   end,
 }
