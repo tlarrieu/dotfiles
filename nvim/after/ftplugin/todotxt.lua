b5f3509d517a4fe088e2fbc17759293b545af9ae
Thomas Larrieu <thomas.larrieu@gmail.com>
2025-07-25 19:01:14 +0200

    Remove a bunch of config files we don't use anymore

--- nvim/after/ftplugin/todotxt.lua
+++
@@ -1,7 +0,0 @@
--- Format rules :
--- https://github.com/todotxt/todo.txt?tab=readme-ov-file#todotxt-format-rules
-
-vim.opt_local.iskeyword = vim.opt_local.iskeyword + '@' + '-' + '+'
-vim.opt_local.textwidth = 0
-vim.opt_local.commentstring = 'x %s'
-vim.opt_local.spell = true
