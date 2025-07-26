b49e31fb824d73359f4c8e7235d4110344005b6e
Thomas Larrieu <thomas.larrieu@gmail.com>
2025-07-26 16:35:19 +0200

    Add a new oil keymap

--- nvim/ftplugin/oil.lua
+++ nvim/ftplugin/oil.lua
@@ -1,2 +1,4 @@
 vim.keymap.set('n', '<leader>.', ':lua require("oil").close()<cr>',
   { buffer = true, silent = true, desc = 'Open Oil split' })
+vim.keymap.set('n', 'à', ':lua require("oil").close()<cr>',
+  { buffer = true, silent = true, desc = 'Open Oil split' })
