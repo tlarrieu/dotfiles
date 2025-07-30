47ed2228c644243ebf6ea5f4e1161a2646f5a3ba
Thomas Larrieu <thomas.larrieu@gmail.com>
2025-07-30 15:43:59 +0200

    Disable dashboard screen for now

--- nvim/lua/plugins/dashboard.lua
+++ nvim/lua/plugins/dashboard.lua
@@ -1,23 +1,23 @@
 return {
-  'nvimdev/dashboard-nvim',
-  event = 'VimEnter',
-  opts = {
-    theme = 'hyper',
-    hide = { statusline = false, tabline = false },
-    config = {
-      disable_move = false,
-      header = {
-        [[ ███╗   ██╗██╗   ██╗██╗███╗   ███╗ ]],
-        [[ ████╗  ██║██║   ██║██║████╗ ████║ ]],
-        [[ ██╔██╗ ██║██║   ██║██║██╔████╔██║ ]],
-        [[ ██║╚██╗██║╚██╗ ██╔╝██║██║╚██╔╝██║ ]],
-        [[ ██║ ╚████║ ╚████╔╝ ██║██║ ╚═╝ ██║ ]],
-        [[ ╚═╝  ╚═══╝  ╚═══╝  ╚═╝╚═╝     ╚═╝ ]],
-      },
-      shortcut = {},
-      project = { enable = false },
-      mru = { enable = false },
-      footer = {},
-    },
-  },
+  -- 'nvimdev/dashboard-nvim',
+  -- event = 'VimEnter',
+  -- opts = {
+  --   theme = 'hyper',
+  --   hide = { statusline = false, tabline = false },
+  --   config = {
+  --     disable_move = false,
+  --     header = {
+  --       [[ ███╗   ██╗██╗   ██╗██╗███╗   ███╗ ]],
+  --       [[ ████╗  ██║██║   ██║██║████╗ ████║ ]],
+  --       [[ ██╔██╗ ██║██║   ██║██║██╔████╔██║ ]],
+  --       [[ ██║╚██╗██║╚██╗ ██╔╝██║██║╚██╔╝██║ ]],
+  --       [[ ██║ ╚████║ ╚████╔╝ ██║██║ ╚═╝ ██║ ]],
+  --       [[ ╚═╝  ╚═══╝  ╚═══╝  ╚═╝╚═╝     ╚═╝ ]],
+  --     },
+  --     shortcut = {},
+  --     project = { enable = false },
+  --     mru = { enable = false },
+  --     footer = {},
+  --   },
+  -- },
 }
