f7467c57ddbae7dc2a5751531a0327016f57cb18
Thomas Larrieu <thomas.larrieu@gmail.com>
2025-07-30 13:56:14 +0200

    Update file mapping

--- nvim/after/ftplugin/yaml.lua
+++ nvim/after/ftplugin/yaml.lua
@@ -4,6 +4,9 @@ vim.keymap.set('n', '<c-$>', function()
   require('alternator').alternate({
     ["(.*)/(.*%.?)en%.yml"] = "%1/%2fr.yml",
     ["(.*)/(.*%.?)fr%.yml"] = "%1/%2en.yml",
+
+    ["(.*)/(.*%.?)2025%.yml"] = "%1/%22024.yml",
+    ["(.*)/(.*%.?)2024%.yml"] = "%1/%22025.yml",
   })
 end, { silent = true, buffer = true })
 
