182439c79e5087118f7f5b35ffbf133812f0fddc
Thomas Larrieu <thomas.larrieu@gmail.com>
2025-08-01 10:22:03 +0200

    Tweak markview

--- nvim/lua/plugins/markview.lua
+++ nvim/lua/plugins/markview.lua
@@ -16,7 +16,7 @@ return {
         enable = true,
         hl = "MarkviewInlineCode",
         padding_left = '',
-        padding_right = ''
+        padding_right = '',
       },
     },
     markdown = {
@@ -28,6 +28,7 @@ return {
         style = 'block',
 
         label_direction = 'right',
+        label_hl = 'MarkviewCodeLabel',
 
         border_hl = 'MarkviewCode',
         info_hl = 'MarkviewCodeInfo',
@@ -117,6 +118,72 @@ return {
         marker_parenthesis = {
           add_padding = true,
           conceal_on_checkboxes = true
+        }
+      },
+      horizontal_rules = {
+        enable = true,
+
+        parts = {
+          {
+            type = 'repeating',
+            direction = 'left',
+
+            repeat_amount = function(buffer)
+              local utils = require('markview.utils');
+              local window = utils.buf_getwin(buffer)
+
+              local width = vim.api.nvim_win_get_width(window)
+              local textoff = vim.fn.getwininfo(window)[1].textoff;
+
+              return math.floor((width - textoff - 3) / 2);
+            end,
+
+            text = '-',
+
+            hl = {
+              'MarkviewGradient1', 'MarkviewGradient1',
+              'MarkviewGradient2', 'MarkviewGradient2',
+              'MarkviewGradient3', 'MarkviewGradient3',
+              'MarkviewGradient4', 'MarkviewGradient4',
+              'MarkviewGradient5', 'MarkviewGradient5',
+              'MarkviewGradient6', 'MarkviewGradient6',
+              'MarkviewGradient7', 'MarkviewGradient7',
+              'MarkviewGradient8', 'MarkviewGradient8',
+              'MarkviewGradient9', 'MarkviewGradient9'
+            }
+          },
+          {
+            type = 'text',
+            text = '  ',
+            hl = 'MarkviewIcon3Fg'
+          },
+          {
+            type = 'repeating',
+            direction = 'right',
+
+            repeat_amount = function(buffer)
+              local utils = require('markview.utils');
+              local window = utils.buf_getwin(buffer)
+
+              local width = vim.api.nvim_win_get_width(window)
+              local textoff = vim.fn.getwininfo(window)[1].textoff;
+
+              return math.ceil((width - textoff - 3) / 2);
+            end,
+
+            text = '-',
+            hl = {
+              'MarkviewGradient1', 'MarkviewGradient1',
+              'MarkviewGradient2', 'MarkviewGradient2',
+              'MarkviewGradient3', 'MarkviewGradient3',
+              'MarkviewGradient4', 'MarkviewGradient4',
+              'MarkviewGradient5', 'MarkviewGradient5',
+              'MarkviewGradient6', 'MarkviewGradient6',
+              'MarkviewGradient7', 'MarkviewGradient7',
+              'MarkviewGradient8', 'MarkviewGradient8',
+              'MarkviewGradient9', 'MarkviewGradient9'
+            }
+          }
         }
       },
     },
