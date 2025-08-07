40d7d4df44739029b1bdc06d73e20f46241cefa0
Thomas Larrieu <thomas.larrieu@gmail.com>
2025-08-07 11:38:51 +0200

    Formatting

--- nvim/lua/helpers.lua
+++ nvim/lua/helpers.lua
@@ -1,12 +1,12 @@
-local _M = {}
+local M = {}
 
-_M.basename = function()
+M.basename = function()
   local filename = vim.api.nvim_buf_get_name(0):match("^.+/(.+)$")
   local basename = (filename or ''):match("(.+)[.].+")
   return basename or filename or ""
 end
 
-_M.dirname = function()
+M.dirname = function()
   local filename = vim.api.nvim_buf_get_name(0)
   local dirname = filename:match("^(.+)/.+$")
 
@@ -17,32 +17,32 @@ _M.dirname = function()
   return dirname:match("^.+/(.+)$")
 end
 
-_M.fileexists = function(path)
+M.fileexists = function(path)
   local f = io.open(path, "rb")
   if f then f:close() end
   return f ~= nil
 end
 
-_M.capitalize = function(str)
+M.capitalize = function(str)
   return str:gsub("^%a", string.upper, 1) or str
 end
 
-_M.camelize = function(str)
+M.camelize = function(str)
   if not (str:find("_")) then
     return str:lower()
   end
   return str:gsub('(_)([a-z])', function(_, l) return l:upper() end) or str
 end
 
-_M.pascalize = function(str)
-  return _M.capitalize(_M.camelize(str))
+M.pascalize = function(str)
+  return M.capitalize(M.camelize(str))
 end
 
-_M.snakify = function(str)
+M.snakify = function(str)
   return str:gsub('([a-z])([A-Z])', '%1_%2'):lower() or str
 end
 
-_M.coerce = function(str, width)
+M.coerce = function(str, width)
   local count, res = 0, ''
   for word in str:gmatch('%S+') do
     count = count + #word
@@ -55,8 +55,8 @@ _M.coerce = function(str, width)
   return res
 end
 
-_M.merge = function(left, right)
+M.merge = function(left, right)
   return vim.tbl_deep_extend('keep', left, right)
 end
 
-return _M
+return M
