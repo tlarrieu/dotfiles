85494439c34f08821b2fdcde1194484bc45b52ce
Thomas Larrieu <thomas.larrieu@gmail.com>
2025-08-07 23:25:57 +0200

    Tweak python related configuration

--- nvim/after/ftplugin/python.lua
+++ nvim/after/ftplugin/python.lua
@@ -1,5 +1,7 @@
-local runner = require('runner')
+vim.bo.expandtab = false
+vim.bo.softtabstop = 2
+vim.bo.tabstop = 2
+vim.bo.shiftwidth = 2
 
-runner.default({
-  main = runner.term('python %'),
-})
+local runner = require('runner')
+runner.default({ main = runner.term('python %') })
--- nvim/snippets/python.lua
+++ nvim/snippets/python.lua
@@ -1,26 +1,28 @@
 ---@diagnostic disable: undefined-global
 
 return {
-  -- imports
-  s("req", fmta("import <>", { i(1) })),
-  s("from", fmta("from <> import <>", { i(1, 'modname'), i(2, 'funcname') })),
+	s("#!", fmta("#!/usr/bin/env ruby", {})),
+	-- imports
+	s("req", fmta("import <>", { i(1) })),
+	s("from", fmta("from <> import <>", { i(1, 'modname'), i(2, 'funcname') })),
 
-  -- definitions
-  s("c", fmta([[
-    class <>:
-        <><>
-    ]], { i(1, 'Name'), sel(), i(0) })),
-  s("d", fmta([[
-    def <>(<>):
-        <><>
+	-- definitions
+	s("c", fmta([[
+		class <>:
+			<><>
+		]], { i(1, 'Name'), sel(), i(0) })),
+	s("d", fmta([[
+		def <>(<>):
+			<><>
     ]], { i(1, "name"), i(2), sel(), i(0) })),
-  s("di", fmta([[
-    def __init__(self<>):
-        <><>
-    ]], { i(1), sel(), i(0) })),
-  s("ret", fmta("return ", {})),
+	s("di", fmta([[
+		def __init__(self<>):
+			<><>
+		]], { i(1), sel(), i(0) })),
+	s("ret", fmta("return ", {})),
 
-  -- misc
-  s("p", fmta('print(<>)', { i(1) })),
+	-- misc
+	s("p", fmta('print(<>)', { i(1) })),
 }, {
 }
+-- vim: ts=2 sts=2 sw=2 noet
