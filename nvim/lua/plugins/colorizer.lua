dcf42469602e9049cae86b237be53da62224152c
Thomas Larrieu <thomas.larrieu@gmail.com>
2025-08-10 02:28:41 +0200

    Replace colorizer

--- nvim/lua/plugins/colorizer.lua
+++ nvim/lua/plugins/colorizer.lua
@@ -1,11 +1,11 @@
 return {
-  'norcalli/nvim-colorizer.lua',
-  opts = {
-    'css',
-    'scss',
-    'rasi',
-    'conf',
-    'xdefaults',
-    'Solarized',
-  },
+  'brenoprata10/nvim-highlight-colors',
+  opts = {},
+  config = function(_, opts)
+    require('nvim-highlight-colors').setup(opts)
+    vim.api.nvim_create_autocmd('ColorScheme', {
+      callback = require("nvim-highlight-colors").turnOn,
+      group = vim.api.nvim_create_augroup('colorizer_group', {})
+    })
+  end
 }
