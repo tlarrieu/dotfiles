03ca8aa5515edbb858e0f7c7ee8168a0ae8786aa
Thomas Larrieu <thomas.larrieu@gmail.com>
2025-08-11 14:20:24 +0200

    Override highlight for private_class_method

--- nvim/after/queries/ruby/highlights.scm
+++ nvim/after/queries/ruby/highlights.scm
@@ -2,3 +2,8 @@
 
 (keyword_parameter name: (_) @variable.key)
 (pair key: (_) @variable.key)
+
+(call
+  method: (identifier)
+  @keyword.function.ruby
+  (#eq? @keyword.function.ruby "private_class_method"))
