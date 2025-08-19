f1d6e0bc4cb5b918d6d65a63975fa238c1c8def6
Thomas Larrieu <thomas.larrieu@gmail.com>
2025-08-19 13:55:37 +0200

    Add some buffer specific keymaps for GV

--- nvim/after/ftplugin/GV.lua
+++ nvim/after/ftplugin/GV.lua
@@ -1,1 +1,4 @@
 vim.opt_local.list = false
+
+vim.keymap.set('n', 'ß', '[[', { remap = true, buffer = true, desc = 'Previous hunk' })
+vim.keymap.set('n', 'þ', ']]', { remap = true, buffer = true, desc = 'Next hunk' })
