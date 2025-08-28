b1c0ca47a443a291d5d816c827e1979cd0aaff67
Thomas Larrieu <thomas.larrieu@gmail.com>
2025-08-28 18:19:39 +0200

    Tweak colors

--- nvim/after/queries/gitcommit/highlights.scm
+++ nvim/after/queries/gitcommit/highlights.scm
@@ -2,7 +2,11 @@
 
 (branch) @git.branch
 ((title) @git.title (#eq? @git.title "Changes to be committed:")) @git.title.committed
+((title) @git.title (#eq? @git.title "Changes not staged for commit:")) @git.title.not_committed
 ((title) @git.title (#eq? @git.title "Untracked files:")) @git.title.untracked
 (change kind: (deleted)) @git.change.deleted
+(generated_comment (change kind: (deleted)) (filepath) @git.change.deleted.filepath)
 (change kind: (modified)) @git.change.modified
+(generated_comment (change kind: (modified)) (filepath) @git.change.modified.filepath)
 (change kind: (new)) @git.change.new
+(generated_comment (change kind: (new)) (filepath) @git.change.new.filepath)
