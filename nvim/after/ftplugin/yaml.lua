505c6516bd3d8192c5534ee4fc0e6f5b128ce5c4
Thomas Larrieu <thomas.larrieu@gmail.com>
2025-09-03 11:48:45 +0200

    Small tweak

--- nvim/after/ftplugin/yaml.lua
+++ nvim/after/ftplugin/yaml.lua
@@ -1,14 +1,3 @@
 vim.keymap.set('n', '<c-l>', '<cmd>YAMLTelescope<cr>', { desc = 'Telescope YAML', silent = true, buffer = true })
-
-vim.keymap.set('n', '<c-$>', function()
-  require('alternator').alternate({
-    ["(.*)/(.*%.?)en%.yml"] = "%1/%2fr.yml",
-    ["(.*)/(.*%.?)fr%.yml"] = "%1/%2en.yml",
-
-    ["(.*)/(.*%.?)2025%.yml"] = "%1/%22024.yml",
-    ["(.*)/(.*%.?)2024%.yml"] = "%1/%22025.yml",
-  })
-end, { silent = true, buffer = true })
-
 vim.keymap.set('n', '<leader>yk', '<cmd>YAMLYankKey<cr>', { silent = true, buffer = true })
 vim.keymap.set('n', '<leader>yv', '<cmd>YAMLYankValue<cr>', { silent = true, buffer = true })
