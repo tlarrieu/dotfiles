39aaa6bdb0cb23eacbbc09d236b9a00d37ce06e3
Thomas Larrieu <thomas.larrieu@gmail.com>
2025-09-08 23:31:17 +0200

    Tweak ledger snippets

--- nvim/snippets/ledger.lua
+++ nvim/snippets/ledger.lua
@@ -8,75 +8,31 @@ return {
   <> ! <> | <>
     <>  -<> €
     <>
-  ]], {
-    f(curdate),
-    i(4, 'payee'),
-    i(5, 'desc'),
-    i(2, 'card:ce'),
-    i(1, 'amount'),
-    i(3, 'to')
-  })),
+  ]], { f(curdate), i(4, 'payee'), i(5, 'desc'), i(2, 'card:ce'), i(1, 'amount'), i(3, 'to') })),
   s('r', fmta([[
   <> ! <> | <>
     <>  <> €
     refund
-  ]], {
-    f(curdate),
-    i(1, 'me'),
-    i(2, 'desc'),
-    i(3, 'card:ce'),
-    i(4, 'amount'),
-  })),
+  ]], { f(curdate), i(1, 'me'), i(2, 'desc'), i(3, 'card:ce'), i(4, 'amount') })),
   s('i', fmta([[
   <> * <>
     card:<>  <> €
     salary/<>
-  ]], {
-    f(curdate),
-    i(1, 'me'),
-    i(2, 'ce'),
-    i(3, 'amount'),
-    i(0, 'Thomas')
-  })),
+  ]], { f(curdate), i(1, 'me'), i(2, 'ce'), i(3, 'amount'), i(0, 'Thomas') })),
   s('t', fmta([[
   <> * <> | transfert
     <>  <> €
     <>
-  ]], {
-    f(curdate),
-    i(1, 'me'),
-    i(2, 'to'),
-    i(3, 'amount'),
-    i(0, 'from')
-  })),
+  ]], { f(curdate), i(1, 'me'), i(2, 'to'), i(3, 'amount'), i(0, 'from') })),
+
+  s('l', fmta('<>  <> €', { i(1, 'account'), i(0, 'amount') })),
+
+  -- Specific expenses
   s('g', fmta([[
   <> ! <> | courses
     <>  -<> €
     groceries
-  ]], {
-    f(curdate),
-    i(3, 'grandfrais'),
-    i(2, 'card:bp'),
-    i(1, 'amount'),
-  })),
-
-  s('l', fmta('<>  <> €', {
-    i(1, 'account'),
-    i(0, 'amount')
-  })),
-
-  -- Specific expenses
-  s('m', fmta([[
-  <> * caisse d'épargne | échéance crédit
-    card:ce  -1403.16 €
-    mortgage  1051.63 €
-    bank:loans
-  <> * caisse d'épargne | assurance crédit
-    bank:fees  3.05 €
-    card:ce
-  ]], {
-    f(curdate), f(curdate),
-  })),
+  ]], { f(curdate), i(3, 'grandfrais'), i(2, 'card:bp'), i(1, 'amount') })),
   s('q', fmta([[
   <> * église | quête
     <>  -<> €
@@ -86,5 +42,16 @@ return {
     i(2, 'from'),
     i(1, 'amount')
   })),
+  s('sfr', fmta([[
+  <> * sfr | abonnement téléphone Miriam
+    card:bp  -11.68 €
+    phone
+  ]], { f(curdate) })),
+  s('rent', fmta([[
+  <> * mme Evain | loyer + charges
+    card:ce  -800 €
+    rent      730 €
+    charges   70  €
+  ]], { f(curdate) })),
 }, {
 }
