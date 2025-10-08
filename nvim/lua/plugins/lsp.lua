d61b64355b00b6a701e6a2e63fd8cdc4d1c7eac9
Thomas Larrieu <thomas.larrieu@gmail.com>
2025-10-08 21:04:10 +0200

    Tweak icons

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -9,9 +9,9 @@ return {
           border = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' },
           height = 0.85,
           icons = {
-            package_installed = '●',
-            package_pending = '󰔟',
-            package_uninstalled = '○',
+            package_installed = '',
+            package_pending = '󱫬',
+            package_uninstalled = '',
           },
           keymaps = {
             toggle_package_expand = '<cr>',
