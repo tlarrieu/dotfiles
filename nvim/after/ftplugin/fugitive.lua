a2774d08627e1068dba2c216208ea68b72817745
Thomas Larrieu <thomas.larrieu@gmail.com>
2025-10-14 11:14:17 +0200

    Update fugitive related keymaps

--- nvim/after/ftplugin/fugitive.lua
+++ nvim/after/ftplugin/fugitive.lua
@@ -4,3 +4,16 @@ vim.keymap.set('n', 'k', '(', { remap = true, buffer = true })
 vim.keymap.set('n', 'j', ')', { remap = true, buffer = true })
 vim.keymap.set({ 'n', 'x' }, 'x', 'X', { remap = true, buffer = true })
 vim.keymap.set('n', '<leader>l', '<cmd>bd<cr>', { buffer = true })
+
+vim.keymap.set('n', 'cc', '<cmd>Git commit --quiet<cr>', {
+  desc = 'Git commit',
+  buffer = true,
+})
+vim.keymap.set('n', 'ca', '<cmd>Git commit --quiet --amend --no-edit<cr>', {
+  desc = 'Git commit --amend --no-edit',
+  buffer = true,
+})
+vim.keymap.set('n', 'cA', '<cmd>Git commit --quiet --amend<cr>', {
+  desc = 'Git commit --amend',
+  buffer = true,
+})
