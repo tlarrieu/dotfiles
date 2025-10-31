7969f781e69e48d5c6d6031b94775fb5a070d53f
Thomas Larrieu <thomas.larrieu@gmail.com>
2025-10-31 14:50:26 +0100

    Tweak markdown snippets

--- nvim/snippets/markdown.lua
+++ nvim/snippets/markdown.lua
@@ -1,5 +1,21 @@
 ---@diagnostic disable: undefined-global
 
+local warning = fmta([[
+  >> [!warning]
+  >> <>]], { i(0) })
+local caution = fmta([[
+  >> [!caution]
+  >> <>]], { i(0) })
+local error = fmta([[
+  >> [!error]
+  >> <>]], { i(0) })
+local info = fmta([[
+  >> [!info]
+  >> <>]], { i(0) })
+local question = fmta([[
+  >> [!question]
+  >> <>]], { i(0) })
+
 return {
   -- fence
   rs("^`", fmta([[
@@ -29,25 +45,19 @@ return {
     ]], { sel(), i(0) })),
 
   -- callouts
-  s("warn", fmta([[
-    >> [!warning]
-    >> <>]], { i(0) })),
-  s("caution", fmta([[
-    >> [!caution]
-    >> <>]], { i(0) })),
-  s("error", fmta([[
-    >> [!error]
-    >> <>]], { i(0) })),
-  s("info", fmta([[
-    >> [!info]
-    >> <>]], { i(0) })),
+  s("w", warning),
+  s("W", warning),
+  s("c", caution),
+  s("C", caution),
+  s("e", error),
+  s("E", error),
+  s("i", info),
+  s("I", info),
+  s("q", info),
+  s("Q", question),
 
   -- link / image
   s("[", fmta("[<>](<>)", { i(1, "alttxt"), i(0, "url") })),
   s("!", fmta("![<>](<>)", { i(1, "alttxt"), i(0, "url") })),
-
-  -- formatting
-  s("b", fmta("**<><>**", { sel(), i(0) })),
-  s("i", fmta("_<><>_", { sel(), i(0) })),
 }, {
 }
