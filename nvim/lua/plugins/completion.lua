8fbd041cfad7d2c19c21c5d2ad6af2fe965f2d59
Thomas Larrieu <thomas.larrieu@gmail.com>
2025-11-15 15:39:25 +0100

    Simplify completion for cmdline

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -113,19 +113,6 @@ return {
       })
     })
 
-    cmp.setup.cmdline({ '/', '?', '@' }, {
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = {
-        { name = 'buffer' }
-      },
-    })
-
-    cmp.setup.cmdline(':', {
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = cmp.config.sources({
-        { name = 'path' },
-        { name = 'cmdline' },
-      }),
-    })
+    cmp.setup.cmdline({ '/', '?', '@', ':' }, { mapping = cmp.mapping.preset.cmdline() })
   end
 }
