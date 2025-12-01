6750d7c92fbb18c463029cd0c9f0bba5e3bc961b
Thomas Larrieu <thomas.larrieu@gmail.com>
2025-12-01 21:57:09 +0100

    Tweak golang configuration

--- nvim/after/ftplugin/go.lua
+++ nvim/after/ftplugin/go.lua
@@ -8,4 +8,20 @@ runner.default({
   main = runner.term('go run .'),
 })
 
-require('utils').autoformat('*.go')
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.go",
+  callback = function()
+    local params = vim.lsp.util.make_range_params(0, 'utf-8')
+    params.context = { only = { 'source.organizeImports' } }
+    local result = vim.lsp.buf_request_sync(0, 'textDocument/codeAction', params)
+    for cid, res in pairs(result or {}) do
+      for _, r in pairs(res.result or {}) do
+        if r.edit then
+          local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or 'utf-16'
+          vim.lsp.util.apply_workspace_edit(r.edit, enc)
+        end
+      end
+    end
+    vim.lsp.buf.format({ async = false })
+  end
+})
