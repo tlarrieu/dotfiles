e6bbb54a8ae0f1dfa5ffa0c8d7b7fa4cb0b76116
Thomas Larrieu <thomas.larrieu@gmail.com>
2026-01-27 14:07:22 +0100

    Tweak ruby highlighting

--- nvim/after/queries/ruby/highlights.scm
+++ nvim/after/queries/ruby/highlights.scm
@@ -1,23 +1,33 @@
 ; extends
 
+; variables
 (keyword_parameter name: (_) @variable.key)
 (pair key: (_) @variable.key)
 
+; functions
 (call method: (identifier) @keyword.function.ruby (#eq? @keyword.function.ruby "private_class_method"))
 
+; control flow keywords
 (call method: (identifier) @keyword.raise (#eq? @keyword.raise "raise"))
 ((identifier) @keyword.raise (#eq? @keyword.raise "raise"))
-
-(class name: (constant) @class)
-(module name: (constant) @class)
-(assignment left: (constant) @constant.assignment)
-
-((_) . "?" @operator.ternary.ruby (_) ":"  @operator.ternary.ruby (_))
-
 ("next") @keyword.next (#set! priority 110)
 ("break") @keyword.break (#set! priority 110)
 (unless "end" @keyword.conditional)
 (case "end" @keyword.conditional)
+((_) . "?" @operator.ternary.ruby (_) ":"  @operator.ternary.ruby (_))
+
+(_ method: ((identifier)) @type (#eq? @type "class"))
+
+; class
+(class name: (constant) @class)
+
+; module
+(module name: (constant) @class)
+
+; constants
+(assignment left: (constant) @constant.assignment)
+(_ receiver: (constant) @constant (#set! priority 110))
+
 (singleton_method object: (self) @keyword) ; "self" in singleton method definition
 
 ((comment) @comment.directive (#set! priority 120) (#match? @comment.directive "^# frozen_string_literal: (true|false)"))
