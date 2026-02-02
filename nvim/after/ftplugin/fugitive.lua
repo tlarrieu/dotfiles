dfc8f255d9ebb14a5625efc4c00e90abf2ff4ccf
Thomas Larrieu <thomas.larrieu@gmail.com>
2026-02-02 09:31:20 +0100

    Tweak fugitive keymaps

--- nvim/after/ftplugin/fugitive.lua
+++ nvim/after/ftplugin/fugitive.lua
@@ -1,11 +1,11 @@
-vim.keymap.set('n', 'j', ']m', { remap = true, buffer = true }) -- previous file
-vim.keymap.set('n', 'k', '[m', { remap = true, buffer = true }) -- next file
-vim.keymap.set('n', 'p', 'dd', { remap = true, buffer = true }) -- split view diff
-vim.keymap.set('n', 'gp', '=', { remap = true, buffer = true }) -- diff preview
-vim.keymap.set('n', 'þ', ')', { remap = true, buffer = true })  -- next diff
-vim.keymap.set('n', 'ß', '(', { remap = true, buffer = true })  -- previous diff
-vim.keymap.set({ 'n', 'x' }, 'x', 'X', { remap = true, buffer = true })
-vim.keymap.set('n', '<leader>l', '<cmd>bd<cr>', { buffer = true })
+vim.keymap.set('n', 'j', ']m', { remap = true, buffer = true, desc = 'Prev file (fugitive)' })
+vim.keymap.set('n', 'k', '[m', { remap = true, buffer = true, desc = 'Next file (fugitive)' })
+vim.keymap.set('n', 'p', 'dd', { remap = true, buffer = true, desc = 'Diff (split view) (fugitive)' })
+vim.keymap.set('n', 'gp', '=', { remap = true, buffer = true, desc = 'Diff preview (fugitive)' })
+vim.keymap.set('n', 'þ', ')', { remap = true, buffer = true, desc = 'Next diff (fugitive)' })
+vim.keymap.set('n', 'ß', '(', { remap = true, buffer = true, desc = 'Prev diff (fugitive)' })
+vim.keymap.set({ 'n', 'x' }, 'x', 'X', { remap = true, buffer = true, desc = 'Discard (fugitive)' })
+vim.keymap.set('n', '<leader>l', '<cmd>bd<cr>', { buffer = true, desc = 'Close (fugitive)' })
 
 vim.keymap.set('n', 'cc', '<cmd>Git commit --quiet<cr>', {
   desc = 'Git commit',
