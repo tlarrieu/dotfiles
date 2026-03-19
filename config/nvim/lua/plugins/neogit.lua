57b641968953f9f7532b9d5abf90c5e6e49c1970
Thomas Larrieu <thomas.larrieu@gmail.com>
2026-03-19 23:32:19 +0100

    Tweak neogit config

--- config/nvim/lua/plugins/neogit.lua
+++ config/nvim/lua/plugins/neogit.lua
@@ -8,5 +8,45 @@ return {
   cmd = 'Neogit',
   keys = {
     { '<c-y>', '<cmd>Neogit<cr>', desc = 'Neogit' }
-  }
+  },
+  opts = {
+    signs = {
+      -- { CLOSED, OPENED }
+      hunk = { "", "" },
+      item = { "", "" },
+      section = { "", "" },
+    },
+    integrations = {
+      telescope = true,
+    },
+    graph_style = 'kitty',
+    remember_settings = true,
+    kind = 'tab',
+    status = {
+      show_head_commit_hash = true,
+      recent_commit_count = 10,
+      HEAD_padding = 7,
+      HEAD_folded = false,
+      mode_padding = 1,
+      mode_text = {
+        M = "M",
+        N = "N",
+        A = "A",
+        D = "D",
+        C = "C",
+        U = "U",
+        R = "R",
+        T = "T",
+        DD = "DD",
+        AU = "AU",
+        UD = "UD",
+        UA = "UA",
+        DU = "DU",
+        AA = "AA",
+        UU = "UU",
+        ["?"] = "",
+      },
+    },
+    mappings = {},
+  },
 }
