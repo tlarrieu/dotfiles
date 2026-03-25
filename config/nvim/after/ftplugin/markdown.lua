22f48298ba63c6925b61e916816e1c30c98ec09b
Thomas Larrieu <thomas.larrieu@gmail.com>
2026-03-25 23:11:35 +0100

    Remove mdprev

--- config/nvim/after/ftplugin/markdown.lua
+++ config/nvim/after/ftplugin/markdown.lua
@@ -4,9 +4,6 @@ vim.opt_local.foldlevelstart = 10
 vim.opt_local.spell = vim.bo.buftype ~= 'nofile'
 vim.opt_local.shiftwidth = 2
 
-local runner = require('runner')
-runner.default({ main = runner.term('mdprev w %', { open = false }) })
-
 vim.keymap.set('v', '<leader>b', 'S*gvS*eee', { buffer = true, remap = true })
 vim.keymap.set('v', '<leader>i', 'S_ee', { buffer = true, remap = true })
 vim.keymap.set('v', '<leader>s', 'S~gvS~eee', { buffer = true, remap = true })
