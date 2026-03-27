57c751a1f54f9174dd8c551ddca5afc3450f927f
Thomas Larrieu <thomas.larrieu@gmail.com>
2026-03-27 10:27:33 +0100

    Make ColorScheme callback more robust
    All of this to avoid errors while installing packages upon nvim startup

--- config/nvim/lua/plugins/colors.lua
+++ config/nvim/lua/plugins/colors.lua
@@ -764,8 +764,17 @@ return {
 
     vim.api.nvim_create_autocmd('ColorScheme', {
       callback = function(args)
-        local palette = require('nightfox.palette').load(args.match)
-        require('nvim-web-devicons').set_default_icon('', palette.fg.dim, 0)
+        local ok, palette = pcall(require, 'nightfox.palette')
+        if not ok then return end
+
+        palette = palette.load(args.match)
+        if not palette then return end
+
+        local devicons
+        ok, devicons = pcall(require, 'nvim-web-devicons')
+        if not ok then return end
+        if not palette.fg then return end
+        devicons.set_default_icon('', palette.fg.dim, 0)
       end,
       group = vim.api.nvim_create_augroup('nightfox_group', {})
     })
