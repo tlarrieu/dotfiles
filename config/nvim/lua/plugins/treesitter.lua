0c9dc1196bef5b61f0d92144e29975e7c65ee9fe
Thomas Larrieu <thomas.larrieu@gmail.com>
2026-04-25 18:08:52 +0200

    Refactor treesitter mapping definition

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -42,23 +42,26 @@ return {
           vim.wo.foldmethod = 'expr'
           vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
 
-          local ft = vim.bo[ev.buf].filetype
-          local lazypath = '~/.local/share/nvim/lazy'
-          local localpath = '~/git/dotfiles/config/nvim'
+          local map = function(keys, kind, scope)
+            local basepath
 
-          vim.keymap.set('n', '<leader>eh',
-            function() vim.cmd.vsplit(localpath .. '/after/queries/' .. ft .. '/highlights.scm') end,
-            { desc = 'Edit highlight queries (local)', buffer = true })
-          vim.keymap.set('n', '<leader>et',
-            function() vim.cmd.vsplit(localpath .. '/after/queries/' .. ft .. '/textobjects.scm') end,
-            { desc = 'Edit textobject queries (local)', buffer = true })
+            if scope == 'local' then
+              basepath = '~/git/dotfiles/config/nvim/after/queries/'
+            elseif kind == 'textobjects' then
+              basepath = '~/.local/share/nvim/lazy/nvim-treesitter-textobjects/queries/'
+            else
+              basepath = '~/.local/share/nvim/lazy/nvim-treesitter/runtime/queries/'
+            end
 
-          vim.keymap.set('n', '<leader>eH',
-            function() vim.cmd.vsplit(lazypath .. '/nvim-treesitter/runtime/queries/' .. ft .. '/highlights.scm') end,
-            { desc = 'Edit highlight queries (global)', buffer = true })
-          vim.keymap.set('n', '<leader>eT',
-            function() vim.cmd.vsplit(lazypath .. '/nvim-treesitter-textobjects/queries/' .. ft .. '/textobjects.scm') end,
-            { desc = 'Edit textobjcets queries (global)', buffer = true })
+            vim.keymap.set('n', keys,
+              function() vim.cmd.vsplit(basepath .. vim.bo[ev.buf].filetype .. '/' .. kind .. '.scm') end,
+              { desc = 'Edit ' .. kind .. ' queries (' .. scope .. ')', buffer = true })
+          end
+
+          map('<leader>eh', 'highlights', 'local')
+          map('<leader>et', 'textobjects', 'local')
+          map('<leader>eH', 'highlights', 'global')
+          map('<leader>eT', 'textobjects', 'global')
         end,
       })
     end
