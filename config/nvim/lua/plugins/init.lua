4a15c7f8abe7a3dc75e7917415d16f3888972e83
Thomas Larrieu <thomas.larrieu@gmail.com>
2026-04-27 10:44:40 +0200

    Cleanup

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -1,7 +1,5 @@
 return {
   { 'AndrewRadev/linediff.vim', cmd = 'Linediff' },
-  { 'AndrewRadev/switch.vim', },
-  { 'tlarrieu/vim-sniper' },
   { 'tommcdo/vim-exchange' },
   { 'tpope/vim-eunuch', cmd = { 'Delete', 'Mkdir', 'Move', 'Remove', 'Rename', 'SudoWrite', } },
   { 'tpope/vim-projectionist' },
