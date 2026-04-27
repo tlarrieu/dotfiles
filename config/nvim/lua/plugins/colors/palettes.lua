6a447b1027f6ed3874f5bec979b7b0e5ca8345eb
Thomas Larrieu <thomas.larrieu@gmail.com>
2026-04-27 14:27:53 +0200

    Explode colors to different files

---
+++ config/nvim/lua/plugins/colors/palettes.lua
@@ -0,0 +1,70 @@
+return {
+  dawnfox = {
+    black   = { base = '#575279', dim = '#cec8cd', dimmer = '#ebe5e2' },
+    blue    = { base = '#3385a5', dim = '#c4d6d9', dimmer = '#e8eae6' },
+    cyan    = { base = '#56949f', dim = '#cddad8', dimmer = '#ebebe6' },
+    green   = { base = '#82a561', dim = '#d9dec7', dimmer = '#efede0' },
+    magenta = { base = '#907aa9', dim = '#ddd3da', dimmer = '#f0e9e7' },
+    orange  = { base = '#ea9d34', dim = '#f6dcbb', dimmer = '#f9ecdc' },
+    pink    = { base = '#d7827e', dim = '#f0d5cf', dimmer = '#f4dfd9' },
+    red     = { base = '#b4637a', dim = '#e7ccce', dimmer = '#f4e7e3' },
+    white   = { base = '#faf4ed', dim = '#faf4ed', dimmer = '#faf4ed' },
+    yellow  = { base = '#d3a634', dim = '#efdfbb', dimmer = '#f6eddc' },
+
+    comment = '#797593',
+
+    fg      = {
+      base   = '#575279',
+      dim    = '#797593',
+      dimmer = '#9d9aa5',
+    },
+
+    bg0     = '#f2e9e1',     -- Dark bg (status line and float)
+    bg1     = '#faf4ed',     -- Default bg
+    bg2     = '#fffaf3',     -- Lighter bg (colorcolm folds)
+    bg3     = '#f4ede8',     -- Lighter bg (cursor line)
+    bg4     = '#bdbfc9',     -- Conceal, border fg
+
+    fg0     = '#797593',     -- Lighter fg
+    fg1     = '#575279',     -- Default fg
+    fg2     = '#797593',     -- Darker fg (status line)
+    fg3     = '#9d9aa5',     -- Darker fg (line numbers, fold colums)
+
+    sel0    = '#dfdad9',     -- Popup bg, visual selection bg
+    sel1    = '#cecacd',     -- Popup sel bg, search bg
+  },
+  nordfox = {
+    black   = { base = '#232136', dim = '#232136', dimmer = '#232136' },
+    blue    = { base = '#69a6bf', dim = '#36455b', dimmer = '#292d42' },
+    cyan    = { base = '#8fb8bf', dim = '#404a5b', dimmer = '#2d2f42' },
+    green   = { base = '#a3be8c', dim = '#464c4d', dimmer = '#2f2f3e' },
+    magenta = { base = '#c4a7e7', dim = '#4f4666', dimmer = '#322d46' },
+    orange  = { base = '#f6c177', dim = '#5d4d48', dimmer = '#36303c' },
+    pink    = { base = '#ea9a97', dim = '#594250', dimmer = '#352c3f' },
+    red     = { base = '#eb6f92', dim = '#5a364f', dimmer = '#35283e' },
+    white   = { base = '#c2c0d3', dim = '#4e4c61', dimmer = '#312f44' },
+    yellow  = { base = '#ebcb8b', dim = '#5a4f4d', dimmer = '#35303e' },
+
+    comment = '#908caa',
+
+    fg      = {
+      base   = '#c2c0d3',
+      dim    = '#908caa',
+      dimmer = '#6e6a86',
+    },
+
+    bg0     = '#393552',     -- Dark bg (status line and float)
+    bg1     = '#232136',     -- Default bg
+    bg2     = '#2a273f',     -- Lighter bg (colorcolm folds)
+    bg3     = '#2a283e',     -- Lighter bg (cursor line)
+    bg4     = '#5a657d',     -- Conceal, border fg
+
+    fg0     = '#908caa',     -- Lighter fg
+    fg1     = '#c2c0d3',     -- Default fg
+    fg2     = '#908caa',     -- Darker fg (status line)
+    fg3     = '#6e6a86',     -- Darker fg (line numbers, fold colums)
+
+    sel0    = '#44415a',     -- Popup bg, visual selection bg
+    sel1    = '#56526e',     -- Popup sel bg, search bg
+  },
+}
