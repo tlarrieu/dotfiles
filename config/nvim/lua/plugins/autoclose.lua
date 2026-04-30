9568477cfdd0d432d50deed7fdc0725b2623f91a
Thomas Larrieu <thomas.larrieu@gmail.com>
2026-04-30 18:35:25 +0200

    Tweak autoclose

--- config/nvim/lua/plugins/autoclose.lua
+++ config/nvim/lua/plugins/autoclose.lua
@@ -1,4 +1,14 @@
 return {
   'm4xshen/autoclose.nvim',
-  config = true,
+  config = {
+    keys = {
+      ['$'] = { escape = true, close = true, pair = '$$', enabled_filetypes = { 'tex' } },
+      ['<'] = { escape = true, close = true, pair = '<>', enabled_filetypes = { 'html', 'embedded_template', 'xml' } },
+    },
+    options = {
+      disabled_filetypes = { 'TelescopePrompt' },
+      disable_when_touch = true,
+      disable_command_mode = true,
+    },
+  },
 }
