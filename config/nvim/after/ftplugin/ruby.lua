d8d0552a60e4c794efd8e57c7cc72902400fc923
Thomas Larrieu <thomas.larrieu@gmail.com>
2026-05-05 20:30:38 +0200

    Make test runner more generic

--- config/nvim/after/ftplugin/ruby.lua
+++ config/nvim/after/ftplugin/ruby.lua
@@ -10,12 +10,12 @@ local testbus = require('testbus')
 local warn = function(msg) vim.notify(msg, vim.log.levels.WARN) end
 local info = function(msg) vim.notify(msg, vim.log.levels.INFO) end
 
-vim.g.testbus = vim.g.testbus or {}
+vim.g.runner = vim.g.runner or {}
 
 local set = function(key, value)
-  local _state = vim.g.testbus
+  local _state = vim.g.runner
   if value then _state[key] = value end
-  vim.g.testbus = _state
+  vim.g.runner = _state
   return _state[key]
 end
 
@@ -32,11 +32,15 @@ local state = {
   end,
   cmd = function(cmd) return set('cmd', cmd) end,
   pid = function(id) return set('pid', id) end,
+  config = function(cfg) return set('config', cfg) or {} end,
   delete = function(key)
-    assert(key == 'buffer' or key == 'window' or key == 'cmd' or key == 'pid')
-    local _state = vim.g.testbus
+    assert(
+      key == 'buffer' or key == 'window' or key == 'cmd' or key == 'pid' or key == 'config',
+      'Invalid state key: ' .. key
+    )
+    local _state = vim.g.runner
     _state[key] = nil
-    vim.g.testbus = _state
+    vim.g.runner = _state
   end,
 }
 
@@ -44,15 +48,15 @@ local function window()
   if not state.buffer() then
     state.buffer(vim.api.nvim_create_buf(true, true))
     vim.keymap.set('n', '<c-.>', '<cmd>:silent close!<cr>',
-      { desc = 'Close test results', silent = true, buffer = state.buffer() })
+      { desc = 'Close task results', silent = true, buffer = state.buffer() })
     vim.keymap.set({ 'i', 't' }, '<c-.>', '<esc><cmd>:silent close!<cr>',
-      { desc = 'Close test results', silent = true, buffer = state.buffer() })
+      { desc = 'Close task results', silent = true, buffer = state.buffer() })
   end
 
   if state.window() then return state.window() end
 
   state.window(vim.api.nvim_open_win(state.buffer(), false, { split = 'right' }))
-  vim.wo[state.window()].winbar = ' RSpec'
+  vim.wo[state.window()].winbar = state.config().winbar
   return state.window()
 end
 
@@ -68,33 +72,54 @@ local rspec = function(opts)
   local location = fname .. (opts.at_cursor and (':' .. line) or '')
   if location then table.insert(cmd, location) end
 
-  return state.cmd(cmd)
+  return {
+    on_start = testbus.start,
+    on_stdout = testbus.redraw,
+    on_interrupt = testbus.interrupt,
+    on_clean = function()
+      testbus.interrupt()
+      testbus.clear()
+    end,
+    on_bufenter = function() if testbus.is_awaiting() then vim.cmd.startinsert() end end,
+    winbar = ' RSpec',
+    cmd = cmd,
+  }
 end
 
-local run = function(cmd)
-  if cmd == nil then return warn('No test was previously run') end
+local run = function(config)
+  if config then config = state.config(config) else config = state.config() end
 
-  testbus.start(function() end)
+  if config.cmd == nil then return warn('󱈸 No previous job found.') end
+  if state.pid() then return warn(' Job already running...') end
+
+  config.on_start = config.on_start or function() end
+  config.on_stdout = config.on_stdout or function(_) end
+  config.on_interrupt = config.on_interrupt or function() end
+  config.on_clean = config.on_clean or function() end
 
   local opened = state.window() ~= nil
 
-  vim.api.nvim_win_call(window(), function()
-    if state.pid() then return warn('Test is already running') end
+  config.on_start()
 
-    info('󰔟 Tests started...')
+  vim.api.nvim_win_call(window(), function()
+    info('󰔟 Job started...')
 
     vim.bo[state.buffer()].modified = false
     vim.bo[state.buffer()].modifiable = false
+
+    vim.api.nvim_create_autocmd('BufEnter', { callback = config.on_bufenter, buffer = state.buffer() })
+    vim.api.nvim_create_autocmd('BufDelete', { callback = config.on_clean, buffer = state.buffer() })
+
     state.pid(
-      vim.fn.jobstart(cmd, {
+      vim.fn.jobstart(config.cmd, {
         term = true,
-        on_stdout = function(_, data, _) testbus.redraw(data) end,
+        on_stdout = function(_, data, _) config.on_stdout(data) end,
         on_exit = function(_, data, _)
           if data > 0 then
-            testbus.interrupt()
-            warn('󰚎 Tests interrupted')
+            config.on_interrupt()
+            warn('󰚎 Job stopped.')
           else
-            info('󱦟 Tests ended')
+            info('󱦟 Job complete.')
           end
 
           if state.window() ~= nil then
@@ -107,17 +132,6 @@ local run = function(cmd)
       })
     )
     if not opened then vim.api.nvim_win_close(0, false) end
-
-    vim.keymap.set('n', '<leader>ts', function()
-      pcall(vim.fn.jobstop, state.pid())
-      testbus.clear()
-      testbus.interrupt()
-    end, { silent = true })
-
-    vim.keymap.set('n', '<c-.>', function()
-      vim.api.nvim_set_current_win(window())
-      if testbus.is_awaiting() then vim.cmd.startinsert() end
-    end, { desc = 'Show test results' })
   end)
 end
 
@@ -132,6 +146,15 @@ else
   vim.keymap.set('n', '<leader>tf', function() warn('Not a test file') end,
     { desc = 'Run test file', buffer = true })
 end
-vim.keymap.set('n', '<c-.>', function() warn('No test was previously run') end,
-  { desc = 'Show test results' })
-vim.keymap.set('n', '<leader>tl', function() run(state.cmd()) end, { desc = 'Run test file' })
+
+vim.keymap.set('n', '<c-.>', function() vim.api.nvim_set_current_win(window()) end, { desc = 'Show run results' })
+vim.keymap.set('n', '<leader>ts', function()
+  local pid = state.pid()
+  if pid then
+    vim.fn.jobstop(pid)
+    state.config().on_clean()
+  else
+    warn('No running job.')
+  end
+end, { silent = true })
+vim.keymap.set('n', '<leader>tl', function() run() end, { desc = 'Re-start last run' })
