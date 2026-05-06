8dac9117b1a083bfb7d24ba092b67d0f8c7b4cb3
Thomas Larrieu <thomas.larrieu@gmail.com>
2026-05-06 10:09:50 +0200

    Start cleaning up old runner

--- config/nvim/ftplugin/ruby.lua
+++ config/nvim/ftplugin/ruby.lua
@@ -1,7 +1,59 @@
-local runner = require('runner')
-runner.default({ main = runner.term('ruby %', { open = false }) })
-runner.match({ 'Gemfile', '*.gemspec' }, { main = runner.term('bundle') })
-runner.match('config/routes.rb', { main = runner.term('rails routes') })
+local o = vim.opt_local
+
+o.conceallevel = 0
+o.concealcursor = 'cni'
+o.iskeyword = o.iskeyword + '?' + '!'
+o.spell = true
+
+local runner = require('runner2')
+
+local rspec = function(opts)
+  local testbus = require('testbus')
+
+  opts = opts or {}
+
+  local fname = vim.api.nvim_buf_get_name(0)
+  local line = vim.api.nvim_win_get_cursor(0)[1]
+
+  local cmd = { 'bundle', 'exec', 'rspec' }
+  for _, option in ipairs(testbus.adapters.rspec.options) do table.insert(cmd, option) end
+
+  local location = fname .. (opts.at_cursor and (':' .. line) or '')
+  if location then table.insert(cmd, location) end
+
+  return {
+    on_start = testbus.start,
+    on_stdout = testbus.redraw,
+    on_interrupt = testbus.interrupt,
+    on_clean = function()
+      testbus.interrupt()
+      testbus.clear()
+    end,
+    on_bufenter = function() if testbus.is_awaiting() then vim.cmd.startinsert() end end,
+    winbar = ' RSpec',
+    cmd = cmd,
+  }
+end
+
+vim.keymap.set('n', '<cr>',
+  function() runner.run({ cmd = { 'ruby', vim.fn.expand('%') } }) end,
+  { desc = 'Execute with ruby', buffer = true })
+
+local bufname = vim.api.nvim_buf_get_name(0)
+if bufname:match('Gemfile') or bufname:match('*.gemspec') then
+  vim.keymap.set('n', '<cr>',
+    function() runner.run({ cmd = { 'bundle', 'install' }, winbar = ' bundle install' }) end,
+    { desc = 'Run bundler', buffer = true })
+elseif bufname:match('config/routes.rb') then
+  vim.keymap.set('n', '<cr>',
+    function() runner.run({ cmd = { 'rails', 'routes' }, winbar = '󰑪 rails routes' }) end,
+    { desc = 'Run rails routes', buffer = true })
+elseif bufname:match('.*_spec.rb') then
+  vim.keymap.set('n', '<leader>tr', function() runner.run(rspec({ at_cursor = true })) end,
+    { desc = 'Run nearest test', buffer = true })
+  vim.keymap.set('n', '<leader>tf', function() runner.run(rspec({ at_cursor = false })) end,
+    { desc = 'Run test file', buffer = true })
+end
 
 require('utils').autoformat({ '*.json.jbuilder', '*.rake', '*.rb', '.pryrc', '.pryrc.local' })
 
