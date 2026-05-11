70f3903f3633305c6a0b1ffca5b5308572e5edb9
Thomas Larrieu <thomas.larrieu@gmail.com>
2026-05-11 09:53:23 +0200

    Tweak snippets tabstops highlights

--- config/nvim/lua/plugins/colors/theme.lua
+++ config/nvim/lua/plugins/colors/theme.lua
@@ -55,8 +55,8 @@ return {
   Title = { fg = 'palette.fg.dim', style = 'bold' },
   Whitespace = { fg = 'palette.fg.dimmer' },
 
-  SnippetTabstop = { fg = 'palette.fg.dimmer', bg = 'palette.bg0' },
-  SnippetTabstopActive = { fg = 'palette.pink', bg = 'palette.pink.dim' },
+  SnippetTabstop = { fg = 'palette.fg.dimmer', bg = 'palette.bg0', style = 'italic' },
+  SnippetTabstopActive = { fg = 'palette.pink', bg = 'palette.pink.dim', style = 'bold' },
 
   ['@comment'] = { link = 'Comment' },
   ['@comment.documentation'] = { link = 'Documentation' },
