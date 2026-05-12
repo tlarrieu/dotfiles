3cb5cd1346630ed43c3561c36b5c16bae6788446
Thomas Larrieu <thomas.larrieu@gmail.com>
2026-05-12 14:54:56 +0200

    Tweak autoclose

--- config/nvim/lua/plugins/autoclose.lua
+++ config/nvim/lua/plugins/autoclose.lua
@@ -6,7 +6,7 @@ return {
       ['<'] = { escape = true, close = true, pair = '<>', enabled_filetypes = { 'html', 'embedded_template', 'xml' } },
     },
     options = {
-      disabled_filetypes = { 'TelescopePrompt', 'ledger' },
+      disabled_filetypes = { 'TelescopePrompt', 'ledger', 'gitcommit' },
       disable_when_touch = true,
       touch_regex = "[%w(%[{%%'\"]",
       disable_command_mode = true,
