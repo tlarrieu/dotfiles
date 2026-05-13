e2f30db22ec918206d05efa675731789df7427a2
Thomas Larrieu <thomas.larrieu@gmail.com>
2026-05-13 09:57:08 +0200

    Tweak highlights

--- config/nvim/after/queries/lua/highlights.scm
+++ config/nvim/after/queries/lua/highlights.scm
@@ -2,3 +2,4 @@
 
 (break_statement) @keyword.break
 (function_call name: (identifier) @keyword.raise (#eq? @keyword.raise "error"))
+(function_call name: (identifier) @keyword.raise (#eq? @keyword.raise "assert"))
