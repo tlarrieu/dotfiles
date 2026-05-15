12c872aff74d2b1264b60ed335f5401bff3e93d1
Thomas Larrieu <thomas.larrieu@gmail.com>
2026-05-15 17:54:03 +0200

    Remove flickering when editing markdown
    Bullets by default seems to be going back and forth between normal and
    insert mode, making markview toggle on and off, creating some flicker
    (most notably with H1s)
    
    This commit fixes that (we're not going to normal mode anymore, but
    rather directly calling the underlying bullet function through lua

--- config/nvim/lua/plugins/bullets.lua
+++ config/nvim/lua/plugins/bullets.lua
@@ -1,13 +1,14 @@
 vim.pack.add({ 'https://github.com/dkarter/bullets.vim' }, { confirm = false })
 
+vim.g.bullets_set_mappings = 0
+
 vim.api.nvim_create_autocmd('FileType', {
   pattern = { 'markdown', 'gitcommit' },
   callback = function()
     local options = { silent = true, buffer = true }
-    vim.keymap.set('n', 'o', '<cmd>InsertNewBullet<cr>', options)
-    vim.keymap.set('n', '<leader>x', '<cmd>ToggleCheckbox<cr>', options)
+    vim.keymap.set('n', 'o', vim.cmd.InsertNewBullet, options)
+    vim.keymap.set('i', '<cr>', vim.cmd.InsertNewBullet, options)
+    vim.keymap.set('n', '<leader>x', vim.cmd.ToggleCheckbox, options)
   end,
   group = vim.api.nvim_create_augroup('bullets_autocmd', {})
 })
-
-vim.g.set_mappings = 0
