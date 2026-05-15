2814fb516e985c9f89a8c517564a4761f5f5ae08
Thomas Larrieu <thomas.larrieu@gmail.com>
2026-05-15 16:38:11 +0200

    Tweak pack.lua

--- config/nvim/lua/pack.lua
+++ config/nvim/lua/pack.lua
@@ -12,5 +12,4 @@ local del = function(pack) vim.pack.del({ pack }) end
 
 vim.api.nvim_create_user_command('PackPurge', purge, { desc = 'Purge all unused packs' })
 vim.api.nvim_create_user_command('PackUpdate', update, { desc = 'Update all packs' })
-vim.api.nvim_create_user_command('PackDel', function(opts) del(opts.args) end,
-  { nargs = '?', desc = 'Delete a pack by name' })
+vim.api.nvim_create_user_command('PackDel', function(opts) del(opts.args) end, { nargs = 1, desc = 'Delete a pack' })
