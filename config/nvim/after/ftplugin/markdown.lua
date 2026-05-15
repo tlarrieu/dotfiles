7737f65648edb2cd53dff90eee628b7462fcd9f4
Thomas Larrieu <thomas.larrieu@gmail.com>
2026-05-15 18:55:27 +0200

    Tweak bullets

--- config/nvim/after/ftplugin/markdown.lua
+++ config/nvim/after/ftplugin/markdown.lua
@@ -1,6 +1,8 @@
 vim.opt_local.formatoptions:append('t')
 vim.opt_local.spell = vim.bo.buftype ~= 'nofile'
 
+vim.opt_local.shiftwidth = 2
+
 vim.keymap.set('v', '<leader>b', 'S*gvS*eee', { buffer = true, remap = true })
 vim.keymap.set('v', '<leader>i', 'S_ee', { buffer = true, remap = true })
 vim.keymap.set('v', '<leader>s', 'S~gvS~eee', { buffer = true, remap = true })
--- config/nvim/lua/plugins/bullets.lua
+++ config/nvim/lua/plugins/bullets.lua
@@ -9,6 +9,12 @@ vim.api.nvim_create_autocmd('FileType', {
     vim.keymap.set('n', 'o', vim.cmd.InsertNewBullet, options)
     vim.keymap.set('i', '<cr>', vim.cmd.InsertNewBullet, options)
     vim.keymap.set('n', '<leader>x', vim.cmd.ToggleCheckbox, options)
+    vim.keymap.set('i', '<c-t>', vim.cmd.BulletDemote, options)
+    vim.keymap.set('i', '<c-d>', vim.cmd.BulletPromote, options)
+    vim.keymap.set('n', '>>', vim.cmd.BulletDemote, options)
+    vim.keymap.set('n', '<<', vim.cmd.BulletPromote, options)
+    vim.keymap.set('x', '>', vim.cmd.BulletDemote, options)
+    vim.keymap.set('x', '<', vim.cmd.BulletPromote, options)
   end,
   group = vim.api.nvim_create_augroup('bullets_autocmd', {})
 })
