b5504b27b8b5d85a28f98296d059d3353480ea25
Thomas Larrieu <thomas.larrieu@gmail.com>
2026-05-17 14:21:43 +0200

    Update plugins

--- config/nvim/ftplugin/nvim-pack.lua
+++ config/nvim/ftplugin/nvim-pack.lua
@@ -1,3 +1,4 @@
+vim.keymap.set('n', 'q', '<cmd>q<cr>', { silent = true, buffer = true })
 vim.keymap.set('n', 'à', '<cmd>q<cr>', { silent = true, buffer = true })
 vim.keymap.set('n', 's', '<cmd>w<cr>', { silent = true, buffer = true })
 vim.keymap.set('n', 'þ', ']]', { remap = true, silent = true, buffer = true })
