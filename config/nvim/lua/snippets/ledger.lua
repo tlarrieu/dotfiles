21fdefe5662eb9d999fece3aafca85aa11a10822
Thomas Larrieu <thomas.larrieu@gmail.com>
2026-05-19 20:01:58 +0200

    Tweak snippets

--- config/nvim/lua/snippets/ledger.lua
+++ config/nvim/lua/snippets/ledger.lua
@@ -17,7 +17,7 @@ $MONTH/${6:$DAY} ${7:!} ${4:payee} | ${5|parking,essence,péage|}
   card:ce:j  -${1:amount} €
   car:${2|parking,gas,toll|}]],
     g = [[
-$MONTH/${6:$DAY} ${7:!} ${4:intermarché} | ${5:courses}
+$MONTH/${6:$DAY} ${7:!} ${4|intermarché,grandfrais,leclerc|} | ${5:courses}
   ${3|card:ce:j,cash|}  -${1:amount} €
   groceries]],
     q = [[
