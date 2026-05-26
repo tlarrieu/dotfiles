1b82dc0c4dfd2cc8e11b4a39d25aa5850227f2c4
Thomas Larrieu <thomas.larrieu@gmail.com>
2026-05-26 18:59:35 +0200

    Tweak oil

--- config/nvim/lua/plug/oil.lua
+++ config/nvim/lua/plug/oil.lua
@@ -103,6 +103,7 @@ vim.api.nvim_create_autocmd('FileType', {
   pattern = { 'oil' },
   callback = function()
     vim.keymap.set('n', '<leader>.', oil.close, { buffer = true, silent = true, desc = 'Close Oil split' })
-    vim.keymap.set('n', 'à', oil.close, { buffer = true, silent = true, desc = 'Close Oil split' })
+    vim.keymap.set('n', 'à', function() oil.close({ exit_if_last_buf = true }) end,
+      { buffer = true, silent = true, desc = 'Close Oil split' })
   end,
 })
