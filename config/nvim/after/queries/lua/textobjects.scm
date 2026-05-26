e53a569af7a4c21a146083033209db704772812c
Thomas Larrieu <thomas.larrieu@gmail.com>
2026-05-26 20:51:41 +0200

    Tweak lua textobjects

--- config/nvim/after/queries/lua/textobjects.scm
+++ config/nvim/after/queries/lua/textobjects.scm
@@ -3,3 +3,4 @@
 ; @assignment ------------------------------------------------------------------
 
 (field name: (_) @assignment.lhs value: (_) @assignment.rhs)
+(binary_expression left: (_) @assignment.lhs right: (_) @assignment.rhs)
