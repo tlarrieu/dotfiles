23c65b8adc2885ca09689d934a6c77625e76dd79
Thomas Larrieu <thomas.larrieu@gmail.com>
2026-05-27 21:41:56 +0200

    Extract color generation

--- config/nvim/lua/colors/palettes.lua
+++ config/nvim/lua/colors/palettes.lua
@@ -1,15 +1,15 @@
 return {
   light = {
-    black   = { base = '#575279', dim = '#cec8cd', dimmer = '#ebe5e2' },
-    blue    = { base = '#3385a5', dim = '#c4d6d9', dimmer = '#e8eae6' },
-    cyan    = { base = '#56949f', dim = '#cddad8', dimmer = '#ebebe6' },
-    green   = { base = '#82a561', dim = '#d9dec7', dimmer = '#efede0' },
-    magenta = { base = '#907aa9', dim = '#ddd3da', dimmer = '#f0e9e7' },
-    orange  = { base = '#ea9d34', dim = '#f6dcbb', dimmer = '#f9ecdc' },
-    pink    = { base = '#d7827e', dim = '#f0d5cf', dimmer = '#f4dfd9' },
-    red     = { base = '#b4637a', dim = '#e7ccce', dimmer = '#f4e7e3' },
-    white   = { base = '#faf4ed', dim = '#faf4ed', dimmer = '#faf4ed' },
-    yellow  = { base = '#d3a634', dim = '#efdfbb', dimmer = '#f6eddc' },
+    black   = '#575279',
+    blue    = '#3385a5',
+    cyan    = '#56949f',
+    green   = '#82a561',
+    magenta = '#907aa9',
+    orange  = '#ea9d34',
+    pink    = '#d7827e',
+    red     = '#b4637a',
+    white   = '#faf4ed',
+    yellow  = '#d3a634',
 
     fg      = {
       base   = '#575279',
@@ -31,16 +31,16 @@ return {
     }
   },
   dark = {
-    black   = { base = '#232136', dim = '#232136', dimmer = '#232136' },
-    blue    = { base = '#69a6bf', dim = '#36455b', dimmer = '#292d42' },
-    cyan    = { base = '#8fb8bf', dim = '#404a5b', dimmer = '#2d2f42' },
-    green   = { base = '#a3be8c', dim = '#464c4d', dimmer = '#2f2f3e' },
-    magenta = { base = '#c4a7e7', dim = '#4f4666', dimmer = '#322d46' },
-    orange  = { base = '#f6c177', dim = '#5d4d48', dimmer = '#36303c' },
-    pink    = { base = '#ea9a97', dim = '#594250', dimmer = '#352c3f' },
-    red     = { base = '#eb6f92', dim = '#5a364f', dimmer = '#35283e' },
-    white   = { base = '#c2c0d3', dim = '#4e4c61', dimmer = '#312f44' },
-    yellow  = { base = '#ebcb8b', dim = '#5a4f4d', dimmer = '#35303e' },
+    black   = '#232136',
+    blue    = '#69a6bf',
+    cyan    = '#8fb8bf',
+    green   = '#a3be8c',
+    magenta = '#c4a7e7',
+    orange  = '#f6c177',
+    pink    = '#ea9a97',
+    red     = '#eb6f92',
+    white   = '#c2c0d3',
+    yellow  = '#ebcb8b',
 
     fg      = {
       base   = '#c2c0d3',
--- config/nvim/lua/plug/nightfox.lua
+++ config/nvim/lua/plug/nightfox.lua
@@ -2,49 +2,15 @@ vim.pack.add({ 'https://github.com/edeneast/nightfox.nvim' }, { confirm = false 
 
 local colors = require('colors')
 
-local setup = function()
-  local palettes = {
-    dawnfox = colors.palettes.light,
-    nordfox = colors.palettes.dark,
-  }
+local palettes = colors.palettes()
 
-  palettes.dawnfox.fg0 = colors.palettes.light.fg.dim
-  palettes.dawnfox.fg1 = colors.palettes.light.fg.base
-  palettes.dawnfox.fg2 = colors.palettes.light.fg.dim
-  palettes.dawnfox.fg3 = colors.palettes.light.fg.dimmer
-
-  palettes.dawnfox.bg0 = colors.palettes.light.bg.dark
-  palettes.dawnfox.bg1 = colors.palettes.light.bg.base
-  palettes.dawnfox.bg2 = colors.palettes.light.bg.dim
-  palettes.dawnfox.bg3 = colors.palettes.light.bg.dimmer
-  palettes.dawnfox.bg4 = colors.palettes.light.bg.border
-
-  palettes.dawnfox.sel0 = colors.palettes.light.sel.base
-  palettes.dawnfox.sel1 = colors.palettes.light.sel.dim
-
-  palettes.nordfox.fg0 = colors.palettes.dark.fg.dim
-  palettes.nordfox.fg1 = colors.palettes.dark.fg.base
-  palettes.nordfox.fg2 = colors.palettes.dark.fg.dim
-  palettes.nordfox.fg3 = colors.palettes.dark.fg.dimmer
-
-  palettes.nordfox.bg0 = colors.palettes.dark.bg.dark
-  palettes.nordfox.bg1 = colors.palettes.dark.bg.base
-  palettes.nordfox.bg2 = colors.palettes.dark.bg.dim
-  palettes.nordfox.bg3 = colors.palettes.dark.bg.dimmer
-  palettes.nordfox.bg4 = colors.palettes.dark.bg.border
-
-  palettes.nordfox.sel0 = colors.palettes.dark.sel.base
-  palettes.nordfox.sel1 = colors.palettes.dark.sel.dim
-
-  require('nightfox').setup({
-    palettes = palettes,
-    groups = { all = colors.theme },
-  })
-end
+require('nightfox').setup({
+  palettes = { dawnfox = palettes.light, nordfox = palettes.dark },
+  groups = { all = colors.theme },
+})
 
 vim.api.nvim_create_autocmd('ColorScheme', {
   callback = function()
-    setup()
     local palette = colors.palette()
     if not palette then return end
 
@@ -54,5 +20,3 @@ vim.api.nvim_create_autocmd('ColorScheme', {
     devicons.set_default_icon('', palette.fg.dim, 0)
   end,
 })
-
-setup()
