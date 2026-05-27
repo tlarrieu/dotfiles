32d883060b74fdfe6f29f850f47bb11738e317ac
Thomas Larrieu <thomas.larrieu@gmail.com>
2026-05-27 18:39:20 +0200

    Tweak ledger snippets

--- config/nvim/lua/snippets/ledger.lua
+++ config/nvim/lua/snippets/ledger.lua
@@ -4,9 +4,9 @@ return {
     l = '\t${1:account}  ${0:amount} €',
 
     e = [[
-$MONTH/${6:$DAY} ${7:!} ${4:payee} | ${5:desc}
+$MONTH/${6:$DAY} ${7:!} ${4|intermarché,grandfrais,leclerc,kazidomi|} | ${5:courses}
   ${3|card:ce:j,cash|}  -${1:amount} €
-  ${2:from}]],
+  ${2:groceries}]],
     i = [[
 $MONTH/${6:$DAY} ${7:*} ${4|me,miriam|} | ${5:desc}
   ${3:card:ce:j}  ${1:amount} €
@@ -16,10 +16,6 @@ $MONTH/${6:$DAY} ${7:*} ${4|me,miriam|} | ${5:desc}
 $MONTH/${6:$DAY} ${7:!} ${4:payee} | ${5|parking,essence,péage|}
   card:ce:j  -${1:amount} €
   car:${2|parking,gas,toll|}]],
-    g = [[
-$MONTH/${6:$DAY} ${7:!} ${4|intermarché,grandfrais,leclerc|} | ${5:courses}
-  ${3|card:ce:j,cash|}  -${1:amount} €
-  groceries]],
     q = [[
 $MONTH/${6:$DAY} ${7:*} église | quête
   ${3|cash,card:ce:j|}  -${1:amount} €
