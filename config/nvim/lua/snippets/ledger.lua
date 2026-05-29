c0400395c5af7c7621b30d3fea59a330c724fb0b
Thomas Larrieu <thomas.larrieu@gmail.com>
2026-05-29 07:56:50 +0200

    Tweak ledger snippets

--- config/nvim/lua/snippets/ledger.lua
+++ config/nvim/lua/snippets/ledger.lua
@@ -4,9 +4,9 @@ return {
     l = '\t${1:account}  ${0:amount} €',
 
     e = [[
-$MONTH/${6:$DAY} ${7:!} ${4|intermarché,grandfrais,jardin du lupin,leclerc,kazidomi|} | ${5:courses}
+$MONTH/${6:$DAY} ${7:!} ${4:payee} | ${5:description}
   ${3|card:ce:j,cash|}  -${1:amount} €
-  ${2:groceries}]],
+  ${2:from}]],
     i = [[
 $MONTH/${6:$DAY} ${7:*} ${4|me,miriam|} | ${5:desc}
   ${3:card:ce:j}  ${1:amount} €
@@ -20,6 +20,10 @@ $MONTH/${6:$DAY} ${7:!} ${4:payee} | ${5|parking,essence,péage|}
 $MONTH/${6:$DAY} ${7:*} église | quête
   ${3|cash,card:ce:j|}  -${1:amount} €
   charities]],
+    g = [[
+$MONTH/${6:$DAY} ${7:!} ${4|intermarché,grandfrais,jardin du lupin,leclerc,kazidomi|} | courses
+  ${3|card:ce:j,cash|}  -${1:amount} €
+  groceries]],
   },
   skeletons = {}
 }
