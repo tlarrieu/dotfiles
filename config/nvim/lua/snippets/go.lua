009ed484ed7ba4c66fcdb727398df329dbee1847
Thomas Larrieu <thomas.larrieu@gmail.com>
2026-05-30 21:29:59 +0200

    Tweak go snippets

--- config/nvim/lua/snippets/go.lua
+++ config/nvim/lua/snippets/go.lua
@@ -3,6 +3,7 @@ return {
     d = 'func ${1:name}(${2:args}) ${3:type} {\n\t$0\n}',
     s = 'type ${1:Name} struct {\n\t$0\n}',
     a = 'type ${1:Name} = ${2:type}',
+    p = 'fmt.Println(${1:args})',
   },
   skeletons = {}
 }
