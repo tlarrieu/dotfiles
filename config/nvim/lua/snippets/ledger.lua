0ae0e58360937d2e80fc96b3893fba731d3e1bbe
Thomas Larrieu <thomas.larrieu@gmail.com>
2026-05-30 11:15:44 +0200

    Update ledger snippets

--- config/nvim/lua/snippets/ledger.lua
+++ config/nvim/lua/snippets/ledger.lua
@@ -13,9 +13,9 @@ $MONTH/${6:$DAY} ${7:*} ${4|me,miriam|} | ${5:desc}
   ${2|salary/Thomas,CPAM,refund,transfers|}]],
 
     c = [[
-$MONTH/${6:$DAY} ${7:!} ${4:payee} | ${5|parking,essence,péage|}
+$MONTH/${6:$DAY} ${7:!} ${4:payee} | ${5|parking,essence,lavage,péage|}
   card:ce:j  -${1:amount} €
-  car:${2|parking,gas,toll|}]],
+  car:${2|parking,gas,wash,toll|}]],
     q = [[
 $MONTH/${6:$DAY} ${7:*} église | quête
   ${3|cash,card:ce:j|}  -${1:amount} €
