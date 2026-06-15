e3ef310d8f043622c9ed8f884cb0abfd6b18505a
Thomas Larrieu <thomas.larrieu@gmail.com>
2026-06-15 16:35:56 +0200

    Tweak SQL ftplugin

--- config/nvim/after/ftplugin/sql.lua
+++ config/nvim/after/ftplugin/sql.lua
@@ -3,22 +3,24 @@ local getpos = function(query)
   return res[2], res[3]
 end
 
+local wrap = function(query)
+  return { cmd = { 'psql', '-c', query }, winbar = ' psql' }
+end
+
 local select = function(selector)
-  return function()
-    local cursor = vim.api.nvim_win_get_cursor(0)
-    vim.cmd.normal(selector .. '')
-    local srow, scol = getpos("'<")
-    local erow, ecol = getpos("'>")
-    vim.api.nvim_win_set_cursor(0, cursor)
+  local cursor = vim.api.nvim_win_get_cursor(0)
+  vim.cmd.normal(selector .. '')
+  local srow, scol = getpos("'<")
+  local erow, ecol = getpos("'>")
+  vim.api.nvim_win_set_cursor(0, cursor)
 
-    local query = table.concat(vim.api.nvim_buf_get_text(0, srow - 1, scol - 1, erow - 1, ecol, {}), '\n')
+  local query = table.concat(vim.api.nvim_buf_get_text(0, srow - 1, scol - 1, erow - 1, ecol, {}), '\n')
 
-    return { cmd = { 'psql', '-c', query }, winbar = ' psql' }
-  end
+  return query
 end
 
 require('runner').setup({
-  main = { args = select('vip'), desc = 'psql -f %' },
-  alt = { args = select('vis'), desc = 'psql -f %' },
+  main = { args = function() return wrap(select('vip')) end, desc = 'psql -f %' },
+  alt = { args = function() return wrap('\\d ' .. select('viw')) end, desc = 'psql -f %' },
   repl = { args = { cmd = 'psql', winbar = ' psql' } },
 })
