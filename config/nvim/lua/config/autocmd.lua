caca1825c62192c0568acd878ef2a3c65512686e
Thomas Larrieu <thomas.larrieu@gmail.com>
2026-06-17 21:16:28 +0200

    Minor tweak

--- config/nvim/lua/config/autocmd.lua
+++ config/nvim/lua/config/autocmd.lua
@@ -38,4 +38,4 @@ vim.api.nvim_create_autocmd('BufWinEnter', {
 
 -- auto hide statusline when entering command line
 vim.api.nvim_create_autocmd('CmdlineEnter', { callback = function() vim.opt.laststatus = 0 end })
-vim.api.nvim_create_autocmd('CmdlineLeavePre', { callback = function() vim.opt.laststatus = 3 end })
+vim.api.nvim_create_autocmd('CmdlineLeave', { callback = function() vim.opt.laststatus = 3 end })
