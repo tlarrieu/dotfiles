c16c15019c97428ea4db1ce457bf3b9eefbe7d5b
Thomas Larrieu <thomas.larrieu@gmail.com>
2026-06-17 19:58:13 +0200

    Make pull and push async

--- config/nvim/lua/plug/fugitive.lua
+++ config/nvim/lua/plug/fugitive.lua
@@ -13,10 +13,12 @@ vim.keymap.set('n', '<leader>l', ':vertical Flogsplit<cr>', { silent = true, des
 vim.keymap.set('n', '<leader>L', ':vertical 0Flogsplit -no-patch<cr>', { silent = true, desc = 'Git history (file)' })
 vim.keymap.set('v', '<leader>l', ':vertical Flogsplit -no-patch<cr>', { silent = true, desc = 'Git history (visual)' })
 
-vim.keymap.set('n', '<leader>cc', '<cmd>below Git commit<cr>', { silent = true, desc = 'Git commit (new)' })
+vim.keymap.set('n', '<leader>cc', '<cmd>below Git commit<cr>',
+  { silent = true, desc = 'Git commit (new)' })
 vim.keymap.set('n', '<leader>ca', '<cmd>below Git commit --amend --no-edit<cr>',
   { silent = true, desc = 'Git commit (amend)' })
-vim.keymap.set('n', '<leader>ce', '<cmd>below Git commit --amend<cr>', { silent = true, desc = 'Git commit (edit)' })
+vim.keymap.set('n', '<leader>ce', '<cmd>below Git commit --amend<cr>',
+  { silent = true, desc = 'Git commit (edit)' })
 
 vim.keymap.set({ 'n', 'v' }, 'gy', ':GBrowse!<cr>', { silent = true, desc = 'Git(Hub) yank file/line URL' })
 
@@ -26,9 +28,22 @@ vim.keymap.set('n', '<leader>bl',
   function() require('telescope.builtin').git_branches({ show_remote_tracking_branches = false }) end,
   { desc = 'Git branch (local)' })
 
-vim.keymap.set('n', '<leader>gu', '<cmd>Git pull --rebase<cr>', { silent = true, desc = 'Git pull' })
-vim.keymap.set('n', '<leader>gp', '<cmd>Git push --force-with-lease<cr>', { silent = true, desc = 'Git push' })
---
+local async_run = function(cmd)
+  return function()
+    vim.notify('Running `' .. table.concat(cmd, ' ') .. '`...', vim.log.levels.INFO)
+    vim.system(cmd, {},
+      function(obj)
+        if obj.code == 0 then return vim.notify(obj.stdout, vim.log.levels.INFO) end
+        vim.notify(obj.stderr:gsub('error: ', ''), vim.log.levels.ERROR)
+      end)
+  end
+end
+
+vim.keymap.set('n', '<leader>gu', async_run({ 'git', 'pull', '--rebase' }),
+  { silent = true, desc = 'Git pull' })
+vim.keymap.set('n', '<leader>gp', async_run({ 'git', 'push', '--force-with-lease' }),
+  { silent = true, desc = 'Git pull' })
+
 vim.keymap.set('n', '<leader>gs', ':silent !git stash --quiet<cr><cmd>checktime<cr>',
   { silent = true, desc = 'Git stash' })
 vim.keymap.set('n', '<leader>gS', ':silent !git stash pop --quiet<cr><cmd>checktime<cr>',
