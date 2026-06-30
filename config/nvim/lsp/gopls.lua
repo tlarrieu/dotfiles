2138a9c2d332abe7f5a0a95c517a2bdb0dc3e197
Thomas Larrieu <thomas.larrieu@gmail.com>
2026-06-30 19:23:37 +0200

    Fix gopls configuration

--- config/nvim/lsp/gopls.lua
+++ config/nvim/lsp/gopls.lua
@@ -1,9 +1,12 @@
 return {
+  cmd = { 'gopls' },
+  filetypes = { 'go', 'gomod', 'gowork', 'gotmpl' },
   settings = {
     gopls = {
       gofumpt = true,
       completeUnimported = true,
       usePlaceholders = true,
+      semanticTokens = true,
       analyses = {
         unusedparams = true,
         staticcheck = true,
