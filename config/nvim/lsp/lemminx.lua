08726f8f0bbbf8224bcaf090a480bd5c16f5b4e8
Thomas Larrieu <thomas.larrieu@gmail.com>
2026-08-04 13:15:29 +0200

    Add some missing LSP configurations

---
+++ config/nvim/lsp/lemminx.lua
@@ -0,0 +1,5 @@
+return {
+  cmd = { 'lemminx' },
+  filetypes = { 'xml', 'xslt', 'svg' },
+  root_markers = { '.git' },
+}
---
+++ config/nvim/lsp/yamlls.lua
@@ -0,0 +1,27 @@
+return {
+  cmd = function(dispatchers, config)
+    local cmd = 'yaml-language-server'
+    if (config or {}).root_dir then
+      local local_cmd = vim.fs.joinpath(config.root_dir, 'node_modules/.bin', cmd)
+      if vim.fn.executable(local_cmd) == 1 then
+        cmd = local_cmd
+      end
+    end
+    return vim.lsp.rpc.start({ cmd, '--stdio' }, dispatchers)
+  end,
+  filetypes = { 'yaml' },
+  root_markers = { '.git' },
+  settings = {
+    -- https://github.com/redhat-developer/vscode-redhat-telemetry#how-to-disable-telemetry-reporting
+    redhat = { telemetry = { enabled = false } },
+    -- formatting disabled by default in yaml-language-server; enable it
+    yaml = { format = { enable = true } },
+  },
+  on_init = function(client)
+    --- https://github.com/neovim/nvim-lspconfig/pull/4016
+    --- Since formatting is disabled by default if you check `client:supports_method('textDocument/formatting')`
+    --- during `LspAttach` it will return `false`. This hack sets the capability to `true` to facilitate
+    --- autocmd's which check this capability
+    client.server_capabilities.documentFormattingProvider = true
+  end,
+}
