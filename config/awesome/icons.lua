5ec028a8e0296949c0d90958c390fc788dceda7c
Thomas Larrieu <thomas.larrieu@gmail.com>
2026-08-19 21:31:08 +0200

    Tweak icons

--- config/awesome/icons.lua
+++ config/awesome/icons.lua
@@ -198,6 +198,7 @@ local config = {
   -- 󰺶 Games
   { icon = '󰚤', rule = { name = 'Sir, We Have an Orc Problem' } },
   { icon = '󰆥', rule = { class = 'Tiny Kingdom' } },
+  { icon = '󰓥', rule = { class = 'LootBound.x86_64' } },
   { icon = '󰓥', rule = { class = 'steam_app_3410180' } }, -- Overlooting
   { icon = '󰓥', rule = { class = 'steam_app_1086940' } }, -- Baldur's gate III
   { icon = '󰸓', rule = { class = 'Backpack Battles' } },
