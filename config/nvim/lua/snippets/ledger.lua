54ae8ef38b6dba552033d726f030816134c9c31e
Thomas Larrieu <thomas.larrieu@gmail.com>
2026-09-05 08:56:53 +0200

    Tweak

--- config/nvim/lua/snippets/ledger.lua
+++ config/nvim/lua/snippets/ledger.lua
@@ -5,24 +5,24 @@ return {
 
     e = [[
 $MONTH/${6:$DAY} ${7:!} ${4:payee} | ${5:description}
-  ${3|card:ce:j,cash|}  -${1:amount} €
+  ${3|card:sg:j,cash|}  -${1:amount} €
   ${2:from}]],
     i = [[
 $MONTH/${6:$DAY} ${7:*} ${4|me,miriam|} | ${5:desc}
-  ${3:card:ce:j}  ${1:amount} €
+  ${3:card:sg:j}  ${1:amount} €
   ${2|salary/Thomas,CPAM,CAF,alan,mercer,refund,transfers|}]],
 
     c = [[
 $MONTH/${6:$DAY} ${7:!} ${4:payee} | ${5|parking,essence,lavage,péage|}
-  card:ce:j  -${1:amount} €
+  card:sg:j  -${1:amount} €
   car:${2|parking,gas,wash,toll|}]],
     q = [[
 $MONTH/${6:$DAY} ${7:*} église | quête
-  ${3|cash,card:ce:j|}  -${1:amount} €
+  ${3|cash,card:sg:j|}  -${1:amount} €
   charities]],
     g = [[
 $MONTH/${6:$DAY} ${7:!} ${4|intermarché,grandfrais,jardin du lupin,leclerc,kazidomi|} | courses
-  ${3|card:ce:j,cash|}  -${1:amount} €
+  ${3|card:sg:j,cash|}  -${1:amount} €
   groceries]],
   },
   skeletons = {}
